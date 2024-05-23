51415507c5c33777759030702da67fb6acf15021
Rajesh Chithaluri <rajesh.chithaluri@gspann.com>
2024-05-24 00:05:42 +0530

    issue-3|block-footer|Removing blocks/mini-teasers/mini-teasers.css from build-css.js

--- build-css.js
+++ build-css.js
@@ -31,11 +31,6 @@ const fileMappings = [
     output: './blocks/columns/columns.css',
     wrapper: 'columns-wrapper',
   },
-  {
-    content: './blocks/mini-teasers/mini-teasers.js',
-    output: './blocks/mini-teasers/mini-teasers.css',
-    wrapper: 'mini-teasers-wrapper',
-  },
 ];
 
 const watch = process.argv[2];
