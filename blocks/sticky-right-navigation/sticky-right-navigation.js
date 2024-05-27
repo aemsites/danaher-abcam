b1e5e7057d1b7e04d146b599d08bd103214622a3
Naveen Ramgiri <naveen.ramgiri@gspann.com>
2024-05-27 15:26:00 +0530

    Made changes as requested by Amlan

--- blocks/sticky-right-navigation/sticky-right-navigation.js
+++ blocks/sticky-right-navigation/sticky-right-navigation.js
@@ -1,13 +1,12 @@
+import { ul, li, a } from '../../scripts/dom-builder.js';
+
 export default function decorate(block) {
   const rightNavLinks = document.querySelectorAll('h2');
-  const navLinksUl = document.createElement('ul');
-  navLinksUl.classList.add(...'sticky-ul max-[768px]:hidden'.split(' '));
+  const navLinksUl = ul({class: 'sticky-ul max-[768px]:hidden'});
   rightNavLinks.forEach((rightNavLink) => {
-    const liElement = document.createElement('li');
-    liElement.classList.add(...'w-72 rounded-3xl mb-2 pr-3.5 pl-3.5 py-2.5 hover:bg-[#f2f2f2]'.split(' '));
+    const liElement = li({class: 'w-72 rounded-3xl mb-2 pr-3.5 pl-3.5 py-2.5 hover:bg-[#f2f2f2]'});
 
-    const anchorElement = document.createElement('a');
-    anchorElement.classList.add(...'text-base text-black font-bold'.split(' '));
+    const anchorElement = a({class: 'text-base text-black font-bold'});
 
     anchorElement.textContent = rightNavLink.textContent;
     anchorElement.href = `#${rightNavLink.id}`;
