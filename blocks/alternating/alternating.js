655a1cfe3478422be814d113d583363c447ce867
vijayasri palivela <vspalivela@nautilus.com>
2024-05-31 13:27:17 +0530

    lint-issues

--- blocks/alternating/alternating.js
+++ blocks/alternating/alternating.js
@@ -8,11 +8,11 @@ export default function decorate() {
       innerWrapper.classList.add(...'lg:flex bg-neutral-100 sm:block mx-auto bg-grey-5 max-w-[1120px]'.split(' '));
       innerWrapper.querySelectorAll('div').forEach((row, index) => {
         row.classList.add('basis-1/2');
-        if(index === 0) {
+        if (index === 0) {
           row.classList.add(...'self-center px-10 py-16 lg:pb-15 md:pl-[62px]'.split(' '));
           row.querySelector('h2')?.classList.add(...'title mb-6 text-4xl font-bold text-heading-large font-header'.split(' '));
           row.querySelectorAll('p')?.forEach((p) => {
-            if(p.className.includes('button-container')){
+            if (p.className.includes('button-container')) {
               p.querySelector('a')?.classList.add(...'rounded-2xl text-white text-xs justify-center px-4 font-semibold py-2 w-16 leading-4 items-center tracking-wider leading-10 bg-[#378189] hover:bg-[#2A5F65]'.split(' '));
             } else {
               p.classList.add(...'description text-lg mb-8 font-normal'.split(' '));
