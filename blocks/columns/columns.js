754329f391c35220ea98f1e5ddff5c40a8c067ec
Rajesh-GSPANN <rajesh.chithaluri@gspann.com>
2024-06-07 18:50:58 +0530

    issue-40|Lint fixes

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,21 +2,20 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { div } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
-  
   const isBottomImg = block.classList.contains('md-bottom-image');
   const isTopImg = block.classList.contains('md-top-image');
 
   // setup image columns and other decorations
   [...block.children].forEach((row) => {
-    row.classList.add('flex','md:flex-row','gap-8');
+    row.classList.add('flex', 'md:flex-row', 'gap-8');
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
         const moreStoriesImg = pic.querySelector('img');
         const optimizedPic = createOptimizedPicture(moreStoriesImg.src, 'img-alt', false, [{ width: '750' }]);
         pic.replaceWith(optimizedPic);
-      } 
-      //Image Position 
+      }
+      // Image Position
       if (isBottomImg) {
         row.classList.add(row.firstElementChild?.querySelector('picture') ? 'flex-col-reverse' : 'flex-col');
       } else if (isTopImg) {
