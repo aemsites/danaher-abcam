3569f66ab8772eebe7ace813c968e0877e11a1c5
vijayasri palivela <vspalivela@nautilus.com>
2024-06-12 14:46:42 +0530

    issue#45||code change for padding at bottom of the template

--- templates/blog-page/blog-page.js
+++ templates/blog-page/blog-page.js
@@ -5,7 +5,7 @@ export default function buildAutoBlocks(block) {
   const contentBlocks = block.querySelectorAll('.section');
   const defaultTemplate = div({ id: 'content-wrapper' });
 
-  const content = div({ class: 'main lg:px-16 sm:px-10 px-5' });
+  const content = div({ class: 'main lg:px-16 sm:px-10 pb-10 px-5' });
   const title = getMetadata('og:title');
   const description = getMetadata('og:description');
 
@@ -13,7 +13,7 @@ export default function buildAutoBlocks(block) {
     { class: 'text-center m-auto lg:pt-14 lg:pb-10 sm:pb-[40px]' },
     div(
       { class: 'py-16 font-sans text-base flex flex-col text-center m-auto max-w-[800px] md:px-5' },
-      h1({ class: 'my-5 text-black-0 lg:text-7xl sm:text-5xl mb-8 font-semibold tracking-normal text-3xl' }, title),
+      h1({ class: 'my-5 text-black-0 lg:text-7xl sm:text-[50px] mb-8 font-semibold tracking-normal text-3xl' }, title),
       p({ class: 'tracking-normal lg:text-[27px] font-light text-slate-800 text-xl md:max-w-[800px] w-full' }, description),
 
     ),
