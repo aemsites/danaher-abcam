b320978a67ad3ac9d8c01b00208d1fa51a67dd7b
vijayasri palivela <vspalivela@nautilus.com>
2024-06-19 22:28:10 +0530

    lintissues

--- templates/blog-page/blog-page.js
+++ templates/blog-page/blog-page.js
@@ -1,6 +1,5 @@
 import { buildBlock } from '../../scripts/aem.js';
-import { div } from '../../scripts/dom-builder.js';
- 
+import { div } from '../../scripts/dom-builder.js'; 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const section = main.querySelector('div');
