f5b04b6fc326f23d9609acacfe9d674e47792683
vijayasri palivela <vspalivela@nautilus.com>
2024-06-19 22:22:46 +0530

    newtemplatecode

--- templates/blog-page/blog-page.js
+++ templates/blog-page/blog-page.js
@@ -1,31 +1,12 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { div, h1, p } from '../../scripts/dom-builder.js';
-
-export default function buildAutoBlocks(block) {
-  const contentBlocks = block.querySelectorAll('.section');
-  const defaultTemplate = div({ id: 'content-wrapper' });
-
-  const content = div({ class: 'main lg:px-16 sm:px-10 pb-10 px-5' });
-  const title = getMetadata('og:title');
-  const description = getMetadata('og:description');
-
-  const headTitle = div(
-    { class: 'text-center m-auto lg:pt-14 lg:pb-10 sm:pb-[40px]' },
-    div(
-      { class: 'py-16 font-sans text-base flex flex-col text-center m-auto max-w-[800px] md:px-5' },
-      h1({ class: 'my-5 text-black-0 lg:text-7xl sm:text-[50px] mb-8 font-semibold tracking-normal text-3xl' }, title),
-      p({ class: 'tracking-normal lg:text-[27px] font-light text-slate-800 text-xl md:max-w-[800px] w-full' }, description),
-
-    ),
-  );
-  content.append(headTitle);
-
-  contentBlocks.forEach((blocks) => {
-    content.appendChild(blocks);
-    blocks.style.display = null;
-  });
-
-  defaultTemplate.appendChild(content);
-  block.appendChild(defaultTemplate);
-  defaultTemplate.appendChild(contentBlocks);
+import { buildBlock } from '../../scripts/aem.js';
+import { div } from '../../scripts/dom-builder.js';
+ 
+export default async function buildAutoBlocks() {
+  const main = document.querySelector('main');
+  const section = main.querySelector('div');
+  section.classList.add('mb-8');
+  const headerSection = div({ class: 'mb-10' });
+  const titleBlock = buildBlock('title-card', { elems: [] });
+  headerSection.append(titleBlock);
+  main.insertBefore(headerSection, section);
 }
