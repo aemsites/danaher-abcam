4bc9da4824c7c26bd3387e85c9ba48b68f1e0b49
Shivangi Singh <shivangi.singh@gspann.com>
2024-06-24 10:00:50 +0530

    LintChnages

--- templates/home-page/home-page.js
+++ templates/home-page/home-page.js
@@ -4,10 +4,10 @@ export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const section = main.querySelector('div');
   const heroSection = section.querySelector('div.hero-container');
-  if(heroSection) {
+  if (heroSection) {
     const heroBlock = buildBlock('hero', { elems: [] });
     main.insertBefore(heroBlock, section);
   } else {
     main.append(section);
-  } 
+  }
 }
