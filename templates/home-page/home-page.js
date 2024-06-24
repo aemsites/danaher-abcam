9ef84ad229c71ad67acb6e1d874a96866970fcfd
Shivangi Singh <shivangi.singh@gspann.com>
2024-06-24 09:58:34 +0530

    EmptyDivChanges

--- templates/home-page/home-page.js
+++ templates/home-page/home-page.js
@@ -3,6 +3,11 @@ import { buildBlock } from '../../scripts/aem.js';
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const section = main.querySelector('div');
-  const heroBlock = buildBlock('hero', { elems: [] });
-  main.insertBefore(heroBlock, section);
+  const heroSection = section.querySelector('div.hero-container');
+  if(heroSection) {
+    const heroBlock = buildBlock('hero', { elems: [] });
+    main.insertBefore(heroBlock, section);
+  } else {
+    main.append(section);
+  } 
 }
