81ce24cdebad2e64c26358a77228a8ff5e6bcd18
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-06-25 18:25:40 +0530

    removed last line main.append(section), not required this line

--- templates/product-detail/product-detail.js
+++ templates/product-detail/product-detail.js
@@ -6,5 +6,4 @@ export default async function buildAutoBlocks() {
   const section = main.querySelector('div');
   const contactInfo = div({ class: 'contactinfo-wrapper flex flex-col py-10 px-[6.25%] max-w-7xl box-content mx-auto justify-center' }, div({ class: 'tracking-wider font-normal text-xs/6 mb-2 text-[#65797C]' }, p(getMetadata('contact-info-1')), p(getMetadata('contact-info-2'))));
   section.append(contactInfo);
-  main.append(section);
 }
