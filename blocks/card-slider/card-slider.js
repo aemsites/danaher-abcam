7af58850497ff5919506f2909fcf1d94465fe272
Shivangi Singh <shivangi.singh@gspann.com>
2024-06-27 22:22:19 +0530

    LatestChanges

--- blocks/card-slider/card-slider.js
+++ blocks/card-slider/card-slider.js
@@ -4,7 +4,7 @@ import {
   h6,
   span,
 } from '../../scripts/dom-builder.js';
-import { createOptimizedPicture, getMetadata } from '../../scripts/aem.js';
+import { createOptimizedPicture, decorateIcons, getMetadata } from '../../scripts/aem.js';
 
 async function fetchPostData() {
   try {
@@ -19,14 +19,13 @@ async function fetchPostData() {
 function createAnniversaryBlogCard(post) {
   const card = div({ class: 'blog-card bg-white overflow-hidden m-2 flex-shrink-0 w-full sm:w-1/2 lg:w-1/3' });
   const image = div(
-    { class: 'blog-card-content relative pb-2/3 absolute inset-0 w-full object-cover' },
+    { class: 'blog-card-content relative pb-2/3 absolute inset-0 w-full h-auto object-cover' },
     a({
       href: post.path,
       title: post.title,
     }, createOptimizedPicture(post.image, post.title)),
   );
-  const pictureTag = image.querySelector('picture');
-  const img = pictureTag.querySelector('img');
+  const img = image.querySelector('picture > img');
   img.setAttribute('loading', 'eager');
   const textContainer = div({ class: 'slider-text py-8' });
   const type = h6(span({ class: 'text-gray-600 text-sm' }, post.category));
@@ -48,7 +47,7 @@ function createAnniversaryBlogCard(post) {
 }
 
 function createAnniversaryBlogs(posts) {
-  const container = div({ class: 'slider-wrapper flex transition-transform duration-300 ease-in-out' });
+  const container = div({ class: 'slider-wrapper flex transition-transform duration-300 ease-in-out overflow-x-auto snap-x snap-mandatory' });
   posts.forEach((post) => {
     const card = createAnniversaryBlogCard(post);
     container.appendChild(card);
@@ -57,8 +56,16 @@ function createAnniversaryBlogs(posts) {
 }
 
 function createSliderNavigation() {
-  const prevButton = div({ class: 'slider-button prev bg-gray-100 text-black p-2 w-10 pl-3.5 rounded-full mx-2' }, '<');
-  const nextButton = div({ class: 'slider-button next bg-gray-100 text-black p-2  w-10 pl-3.5 rounded-full mx-2' }, '>');
+  const prevButton = div(
+    { class: 'slider-button cursor-pointer prev my-8 mx-2 md:block hidden' },
+    span({ class: 'icon icon-left-arrow-enabled' }),
+  );
+  const nextButton = div(
+    { class: 'slider-button cursor-pointer next my-8 mx-2 md:block hidden' },
+    span({ class: 'icon icon-right-arrow-enabled' }),
+  );
+  decorateIcons(prevButton);
+  decorateIcons(nextButton);
   return { prevButton, nextButton };
 }
 
@@ -72,6 +79,23 @@ function initializeSlider(sliderContainer, wrapper, prevButton, nextButton) {
     const slideWidth = slides[0].offsetWidth;
 
     wrapper.style.transform = `translateX(-${index * slideWidth}px)`;
+
+    // Update button states based on index
+    if (index === 0) {
+      prevButton.classList.remove('cursor-pointer');
+      prevButton.classList.add('cursor-default');
+    } else {
+      prevButton.classList.remove('cursor-default');
+      prevButton.classList.add('cursor-pointer');
+    }
+
+    if (index === totalSlides - 1) {
+      nextButton.classList.remove('cursor-pointer');
+      nextButton.classList.add('cursor-default');
+    } else {
+      nextButton.classList.remove('cursor-default');
+      nextButton.classList.add('cursor-pointer');
+    }
   }
 
   prevButton.addEventListener('click', () => {
@@ -95,14 +119,13 @@ function initializeSlider(sliderContainer, wrapper, prevButton, nextButton) {
 function createHeroComponent(post) {
   const hero = div({ class: 'hero p-5 mb-5' });
   const image = div(
-    { class: 'hero-image w-full bg-center mb-5' },
+    { class: 'hero-image w-full h-auto bg-center mb-5' },
     a({
       href: post.path,
       title: post.title,
     }, div({ class: '[&_img]:w-full object-cover' }, createOptimizedPicture(post.image, post.title))),
   );
-  const pictureTag = image.querySelector('picture');
-  const img = pictureTag.querySelector('img');
+  const img = image.querySelector('picture > img');
   img.setAttribute('loading', 'eager');
   const textContainer = div({ class: 'text-center' });
   const type = h6(span({ class: 'text-[#999] text-lg' }, post.category));
@@ -130,14 +153,17 @@ export default async function decorate(block) {
   const sliderContainer = div({ class: 'slider-container relative w-full overflow-hidden py-5' });
   const { prevButton, nextButton } = createSliderNavigation();
 
-  // Create "View All" option
-  const viewAllContainer = div({ class: 'view-all-container flex gap-4 items-center mb-2' });
-  const moreStories = div({ class: 'font-bold w-1/2 text-xl text-black-0 justify-start' }, 'More Stories');
-  const viewAll = div({ class: 'flex w-1/2 justify-end' }, a({ href: '/all-blogs', class: 'view-all block  mr-16 font-bold text-xl text-black-0 hover:underline' }, 'View All'));
-  viewAllContainer.appendChild(moreStories);
-  viewAllContainer.appendChild(viewAll);
-  viewAll.appendChild(prevButton);
-  viewAll.appendChild(nextButton);
+  // // Create "View All" option
+  const viewAllContainer = div(
+    { class: 'view-all-container flex gap-4 items-center mb-2' },
+    div({ class: 'font-bold w-1/2 ml-4 text-xl text-black-0 justify-start' }, 'More Stories'),
+    div(
+      { class: 'flex w-1/2 justify-end items-center' },
+      a({ href: '/all-blogs', class: 'view-all block md:mr-12 mr-4 font-bold text-xl text-black-0 hover:underline' }, 'View All'),
+      prevButton,
+      nextButton,
+    ),
+  );
 
   const blogsContainer = div({ class: 'col recent-blogs' });
   const sortedResults = getMetadata('type');
