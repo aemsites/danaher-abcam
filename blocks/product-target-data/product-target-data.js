7a0cc8c5837fbe95fa72c4b1a4ab21bf85f4db32
Naveen Ramgiri <naveen.ramgiri@gspann.com>
2024-07-05 11:06:55 +0530

    pdp-target-data

--- blocks/product-target-data/product-target-data.js
+++ blocks/product-target-data/product-target-data.js
@@ -1,6 +1,17 @@
 import { getProductResponse } from '../../scripts/search.js';
+import { div, h3, a, span } from '../../scripts/dom-builder.js';
+import { decorateIcons } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   const response = await getProductResponse();
-  block.textContent = 'Product Target Data Placeholder';
+  const targetData = response[0].raw.target;
+  const { href } = window.location;
+  const targetContainer = div({ class: 'py-2 flex flex-col' },
+    h3({ class: 'text-lg mb-3  text-black-0' }, 'Target data'),
+    a({ class: 'w-fit inline-flex items-center underline text-[#378189]', href }, `${targetData}`,
+      span({ class: 'icon icon-share-icon ml-2' }),
+    ),
+  );
+  decorateIcons(targetContainer);
+  block.append(targetContainer);
 }
