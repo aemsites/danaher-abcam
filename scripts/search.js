3f393535cb1c554e0bd5652caf7016c7d81b2b09
rgravitvl <ravitvl83@gmail.com>
2024-07-05 11:27:52 +0530

    Updated for 404

--- scripts/search.js
+++ scripts/search.js
@@ -3,6 +3,9 @@ import { sampleRUM } from './aem.js';
 
 const bearerToken = 'xxb93228c5-e569-44de-8dc2-cbb51525ad67';
 const orgId = 'danahernonproduction1892f3fhz';
+let is404Loaded = false;
+let isFetchCalled = false;
+let results;
 
 /**
  *
@@ -13,20 +16,8 @@ export function getSKU() {
   return sku.pop();
 }
 
-/**
- *
- * @returns Product response JSON
- */
-/* eslint consistent-return: off */
-export async function getProductResponse() {
-  try {
-    let response = JSON.parse(localStorage.getItem('optimus-product-details'));
-    const sku = getSKU();
-    if (response && response.at(0)?.raw.productslug === sku) {
-      return response;
-    }
-    localStorage.removeItem('optimus-product-details');
-
+async function getFullResponse(sku) {
+  if (!isFetchCalled) {
     const url = `https://${orgId}.org.coveo.com/rest/search/v2`;
     const body = {
       aq: `@productslug==${sku}`,
@@ -39,31 +30,56 @@ export async function getProductResponse() {
       authToken: bearerToken,
       body: JSON.stringify(body),
     };
-    const fullResponse = await fetchResponse(config);
+    results = await fetchResponse(config);
+  }
+  isFetchCalled = true;
+  return results;
+}
+
+async function redirect404() {
+  if (!is404Loaded) {
+    await fetch('/404.html')
+      .then((html) => html.text())
+      .then((data) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+        document.head.innerHTML = doc.head.innerHTML;
+        document.querySelector('main').innerHTML = doc.querySelector('main')?.innerHTML;
+        document.title = 'Product Not Found';
+        document.querySelector('h2.error-message').innerText = 'Product Not Found';
+        window.addEventListener('load', () => sampleRUM('404', { source: document.referrer, target: window.location.href }));
+      })
+      .catch((error) => {
+      // eslint-disable-next-line no-console
+        console.error('Error:', error);
+      });
+  }
+  is404Loaded = true;
+}
+
+/**
+ *
+ * @returns Product response JSON
+ */
+/* eslint consistent-return: off */
+export async function getProductResponse() {
+  try {
+    let response = JSON.parse(localStorage.getItem('optimus-product-details'));
+    const sku = getSKU();
+
+    if (response?.at(0)?.raw.productslug === sku) {
+      return response;
+    }
+
+    localStorage.removeItem('optimus-product-details');
+    const fullResponse = await getFullResponse(sku);
 
     if (fullResponse.results.length > 0) {
       response = fullResponse.results;
       localStorage.setItem('optimus-product-details', JSON.stringify(fullResponse.results));
       return response;
     }
-
-    if (!response) {
-      await fetch('/404.html')
-        .then((html) => html.text())
-        .then((data) => {
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(data, 'text/html');
-          document.head.innerHTML = doc.head.innerHTML;
-          document.querySelector('main').innerHTML = doc.querySelector('main')?.innerHTML;
-          document.title = 'Product Not Found';
-          document.querySelector('h2.error-message').innerText = 'Product Not Found';
-          window.addEventListener('load', () => sampleRUM('404', { source: document.referrer, target: window.location.href }));
-        })
-        .catch((error) => {
-          // eslint-disable-next-line no-console
-          console.error('Error:', error);
-        });
-    }
+    await redirect404();
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);
