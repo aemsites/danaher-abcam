888f26abc1e4005421b9b507607a7830af801bce
Rajesh-GSPANN <rajesh.chithaluri@gspann.com>
2024-07-08 15:38:30 +0530

    Product Protocols Implementation[PDP] #100 | Implementation completed

--- blocks/product-protocols/product-protocols.js
+++ blocks/product-protocols/product-protocols.js
@@ -1,7 +1,37 @@
-import { getProductResponse } from '../../scripts/search.js';
+import {
+  div,
+  a,
+  h2,
+  ul,
+  li,
+} from '../../scripts/dom-builder.js';
 
 export default async function decorate(block) {
-  const response = await getProductResponse();
   block.classList.add('border-t-4');
   block.textContent = 'Product protocols Placeholder';
+
+  const { origin } = window.location;
+  const productProtocols = div(
+    { class: 'product-protocols' },
+    h2({ class: 'mt-6 mb-4 text-2xl font-semibold tracking-tighter text-[#2A3C3C]' }, 'Product protocols'),
+    div(
+      { class: 'max-w-md font-normal' },
+      'For this product, it\'s our understanding that no specific protocols are required. You can:',
+      ul(
+        { class: 'pt-3 ml-3 list-disc list-inside text-[#071112] text-lg' },
+        li(
+          { class: 'general-protocols' },
+          'Visit the ',
+          a({ class: 'general-protocols text-[#378189] underline', href: origin.concat('/drafts/protocols') }, 'general protocols'),
+        ),
+        li(
+          { class: 'general-protocols' },
+          'Visit ',
+          a({ class: 'text-[#378189] underline', href: origin.concat('/drafts/technical-resources') }, 'troubleshooting'),
+        ),
+
+      ),
+    ),
+  );
+  block.append(productProtocols);
 }
