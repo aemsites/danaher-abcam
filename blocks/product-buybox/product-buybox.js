ba58d5fc9581c65b9b9c37b27abd18e7b3246e5c
rgravitvl <ravitvl83@gmail.com>
2024-07-10 16:09:05 +0530

    color updated for buybox

--- blocks/product-buybox/product-buybox.js
+++ blocks/product-buybox/product-buybox.js
@@ -1,4 +1,4 @@
 export default async function decorate(block) {
-  block.classList.add(...'hidden lg:block h-1/4 w-auto bg-teal-100 col-span-4'.split(' '));
+  block.classList.add(...'hidden lg:block h-1/4 w-auto bg-[#edf7f6] col-span-4 p-6'.split(' '));
   block.textContent = 'Product Buybox Placeholder';
 }
