cbd748bbfb2f0eb0767d7d5fca98808a8022177d
rgravitvl <ravitvl83@gmail.com>
2024-07-10 15:12:31 +0530

    Fixed width of the template

--- blocks/product-buybox/product-buybox.js
+++ blocks/product-buybox/product-buybox.js
@@ -1,5 +1,4 @@
-
 export default async function decorate(block) {
-  block.classList.add(...'h-1/4 w-auto bg-teal-100 col-span-4'.split(' '));
+  block.classList.add(...'hidden lg:block h-1/4 w-auto bg-teal-100 col-span-4'.split(' '));
   block.textContent = 'Product Buybox Placeholder';
 }
