fb940f1947303acbafd841cdc980a22524b210f6
rgravitvl <ravitvl83@gmail.com>
2024-08-07 14:39:12 +0530

    commenting out hero auto block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -157,7 +157,7 @@ async function decorateTemplates(main) {
  */
 function buildAutoBlocks(main) {
   try {
-    buildHeroBlock(main);
+    // buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
