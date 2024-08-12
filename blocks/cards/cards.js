03279c0028dba6fa7782ab58f77d8e25e21b34f2
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 15:07:10 +0530

    changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -20,7 +20,7 @@ export default function decorate(block) {
       }
       const divContainer = document.createElement('div');
       div.classList.add(...'cards-card-body py-9 px-8 flex flex-col grow'.split(' '));
-      const cardHeading = row.querySelector('li > div > h2');
+      const cardHeading = row.querySelector('h2');
       cardHeading.classList.add(...'card-heading text-2xl tracking-[-0.03em]'.split(' '));
 
       const cardDescription = row.querySelector('p');
