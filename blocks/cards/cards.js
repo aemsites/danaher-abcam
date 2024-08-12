1789c50f9b79e33865ad10bf34ac27f356990fce
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 17:18:36 +0530

    card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,8 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const cardBlocks = document.querySelector('.cards');
-  cardBlocks.classList.add(...'w-4/5 m-auto mb-8'.split(' '));
+  const cardBlocks = document.querySelectorAll('.cards');
+  cardBlocks.forEach((parentDiv) => {
+    parentDiv.classList.add(...'w-4/5 m-auto mb-8'.split(' '));
+  });
 }
