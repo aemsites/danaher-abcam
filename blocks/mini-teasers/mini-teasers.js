bdc51c04c303a18fb96dce8744af373cd6550237
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 15:42:49 +0530

    teaser update

--- blocks/mini-teasers/mini-teasers.js
+++ blocks/mini-teasers/mini-teasers.js
@@ -5,8 +5,9 @@ export default function decorate(block) {
   cardBlocks.forEach((parentDiv) => {
     parentDiv.classList.add(...'bg-black mt-72px pb-88px pt-66px'.split(' '));
   });
-  block.firstElementChild.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
+  //block.firstElementChild.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
   [...block.children].forEach((teasers) => {
+    teasers.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
     [...teasers.children].forEach((teaser) => {
       teaser.classList.add('text-white');
       if (teaser.firstChild) {
