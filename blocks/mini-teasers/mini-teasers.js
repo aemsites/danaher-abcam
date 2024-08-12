a07d314897c966402b4e0c414f11aa043b58f99d
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 15:26:13 +0530

    teaser update

--- blocks/mini-teasers/mini-teasers.js
+++ blocks/mini-teasers/mini-teasers.js
@@ -1,6 +1,10 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  const cardBlocks = document.querySelectorAll('.mini-teasers');
+  cardBlocks.forEach((parentDiv) => {
+    parentDiv.classList.add(...'bg-black mt-72px pb-88px pt-66px'.split(' '));
+  });
   block.firstElementChild.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
   [...block.children].forEach((teasers) => {
     [...teasers.children].forEach((teaser) => {
