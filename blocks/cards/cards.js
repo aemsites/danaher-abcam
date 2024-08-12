96fd093ef88d72134ce83b5829a991cc7c9babd9
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 14:02:24 +0530

    commit

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -11,17 +11,16 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.classList.add(...'cards-li flex flex-col bg-[#e5e7eb]'.split(' '));
-    //const pictureTag = row.querySelector('p > picture');
+    // const pictureTag = row.querySelector('p > picture');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('p > picture')) {
         div.className = 'cards-card-image';
-        const imgTag = pictureTag.querySelector('img');
-          if (imgTag) {
-            imgTag.classList.add('max-[799px]:w-full');
-          }
-      }
-      else div.className = 'cards-card-body py-9 px-8 flex flex-col grow';
+        const imgTag = div.querySelector('p > picture > img');
+        if (imgTag) {
+          imgTag.classList.add('max-[799px]:w-full');
+        }
+      } else div.className = 'cards-card-body py-9 px-8 flex flex-col grow';
     });
     // const cardHeading = row.querySelector('li > div > h2');
     // cardHeading.classList.add(...'card-heading text-2xl tracking-[-0.03em]'.split(' '));
@@ -32,7 +31,6 @@ export default function decorate(block) {
     if (cardDescription) {
       cardDescription.classList.add(...'card-description h-full mt-2.5 mb-3 text-base tracking-wide'.split(' '));
     }
-    const cardLink = row.querySelector('.button-container');
     const cardLink = row.querySelector('.button-container');
     if (cardLink) {
       cardLink.classList.add(...'card-link w-fit text-sm text-white bg-[#2A5F65] hover:bg-[#255159] py-2.5 px-5 rounded-[28px]'.split(' '));
