8229c941710cc3275e2917eb86244ecd213f3f8b
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 15:52:43 +0530

    changes

--- blocks/mini-teasers/mini-teasers.js
+++ blocks/mini-teasers/mini-teasers.js
@@ -5,11 +5,8 @@ export default function decorate(block) {
   teasersBlocks.forEach((parentDiv) => {
     parentDiv.classList.add(...'bg-black mt-72px pb-88px pt-66px'.split(' '));
   });
-  const teaserBlock = document.querySelectorAll('.mini-teaser');
-  teaserBlock.forEach((parentDiv) => {
-    parentDiv.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
-  });
-  //block.firstElementChild.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
+  const div = document.createElement('div');
+  div.classList.add(...'mx-auto bg-black w-4/5 py-20 space-y-11 min-[800px]:flex min-[800px]:space-x-11 min-[800px]:space-y-0'.split(' '));
   [...block.children].forEach((teasers) => {
     [...teasers.children].forEach((teaser) => {
       teaser.classList.add('text-white');
