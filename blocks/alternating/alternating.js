c83b03625bc1b4dc07fef234d917d88a90aea75f
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 13:01:28 +0530

    block changes

--- blocks/alternating/alternating.js
+++ blocks/alternating/alternating.js
@@ -27,7 +27,7 @@ export default function decorate() {
         }
       });
     }
-    wrapper.appendChild(innerWrapper);
+    // wrapper.appendChild(innerWrapper);
   }
   section.appendChild(wrapper);
 }
