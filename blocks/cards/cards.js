80dacd23e55fb0c2620b0894c811f543eebd9748
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-12 18:29:40 +0530

    changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -7,30 +7,22 @@ export default function decorate(block) {
   });
   // const cardsTitle = document.querySelector('h2');
   // cardsTitle.classList.add(...'text-5xl mb-8 mt-[72px]'.split(' '));
-  [...block.children].forEach((row) => {
-    let type = '';
-    const heading = row.querySelector('h2');
+  [...block.children].forEach((row, index) => {
+    if (index === 0) {
+      const heading = row.querySelector('h2');
     if (heading) heading.className = 'card-title text-5xl mb-8 mt-[72px]';
-    const h3Heading = row.querySelector('h3');
-    const typeP = h3Heading?.previousElementSibling;
-    if (typeP) {
-      type = typeP.textContent;
-      typeP.remove();
-      block.classList.add(type.toLowerCase());
-    }
-    //cardWrapper.classList.add(...''.split('cards-ul grid grid-cols-3 gap-11 max-[799px]:grid-cols-1'));
-    [...row.children].forEach((elem) => {
-      //cardWrapper.append(elem);
-      if (elem.querySelector('picture, img')) {
-        elem.className = 'cards-card-image';
-      } else {
-        elem.className = 'cards-card-body py-9 px-8 flex flex-col grow';
+    } else {
+      const cardWrapper = row.querySelector('div');
+      cardWrapper.className = 'cards-li flex flex-col bg-[#e5e7eb]';
+        if (row.querySelector('picture, img')) {
+          row.className = 'cards-card-image';
+        } else {
+          row.className = 'cards-card-body py-9 px-8 flex flex-col grow';
+        }
+        if (row?.querySelector('h3')) row.querySelector('h3').className = 'card-heading text-2xl tracking-[-0.03em]';
+        if (row?.querySelector('div > p')) row.querySelector('div > p').className = 'card-description h-full mt-2.5 mb-3 text-base tracking-wide';
       }
-      if (elem?.querySelector('h3')) elem.querySelector('h3').className = 'card-heading text-2xl tracking-[-0.03em]';
-      if (elem?.querySelector('div > p')) elem.querySelector('div > p').className = 'card-description h-full mt-2.5 mb-3 text-base tracking-wide';
-      //row.append(cardWrapper);
     });
-  });
   block.querySelectorAll('img').forEach((img) => {
     img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
   });
