1712cba1db03322d8a23dfdc8e690822847a4a6f
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-14 19:05:19 +0530

    cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,7 +6,7 @@ export default function decorate(block) {
 
   [...block.children].forEach((row) => {
     const pictureTag = row.querySelector('picture');
-    const cardHeading = row.querySelector('h2');
+    const cardHeading = row.querySelector('h3');
     cardHeading.classList.add(...'card-heading text-2xl tracking-[-0.03em]'.split(' '));
 
     const cardDescription = row.querySelector('p');
