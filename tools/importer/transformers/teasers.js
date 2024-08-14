722ac17353dd6ceef788c945d2b00553e2a7b318
wz914876 <sharanagouda_patil@pall.com>
2024-08-14 11:38:24 +0530

    add items row-wise

--- tools/importer/transformers/teasers.js
+++ tools/importer/transformers/teasers.js
@@ -1,30 +1,29 @@
 const teasers = (main, document) => {
     const mainDivEl = document.getElementById('main-content').nextElementSibling.nextElementSibling;
-    const teasersDivEl = document.createElement('div');
-    const ulEl = mainDivEl?.querySelector('ul');
-    const liEl = ulEl?.querySelectorAll('li');
-    liEl?.forEach((item) => {
+    const ulElement = mainDivEl?.querySelector('ul');
+    const liElements = ulElement?.querySelectorAll('li');
+    const cells = [['teasers']];
+    liElements?.forEach((item) => {
       const linkEl = item.querySelector('a');
       const pEl = item.querySelector('p');
-      const divEl = document.createElement('div');
-      const aEl = document.createElement('a');
+      const teaserDivEl = document.createElement('div');
+      const anchorEl = document.createElement('a');
       const h3 = document.createElement('h3');
       const p = document.createElement('p');
-      aEl.href = linkEl?.href;
+      anchorEl.href = linkEl?.href;
       const linkTitle = linkEl?.textContent;
       h3.textContent = linkTitle;
       const linkDesc = pEl?.textContent;
       p.textContent = linkDesc;
-      aEl.append(h3, p);
-      divEl.append(aEl);
-      teasersDivEl.append(divEl);
+      anchorEl.append(h3);
+      teaserDivEl.append(anchorEl);
+      teaserDivEl.append(p);
+      cells.push([teaserDivEl]);
     });
-    const cells = [['teasers']];
-    cells.push([teasersDivEl]);
     if (cells.length > 0) {
       mainDivEl.innerHTML = '';
       const block = WebImporter.DOMUtils.createTable(cells, document);
-      mainDivEl.append(block, document.createElement('hr'));
+      mainDivEl.append(block);
     }
   };
   export default teasers;
