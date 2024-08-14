e99a0ffa919606dbeacd84f73f889f9607bc8d47
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-14 18:43:34 +0530

    card changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,19 +6,36 @@ import {
 export default function decorate(block) {
   block.classList.add(...'w-4/5 m-auto mb-8'.split(' '));
   const parentWrapper = block.querySelector('div');
+  parentWrapper.classList.add(...'flex flex-col bg-[#e5e7eb]'.split(' '));
   const headingContainer = div({ class: 'cards-card-body py-9 px-8 flex flex-col grow' });
   const imageContainer = div({ class: 'cards-card-image' });
+  const pictureTag = block.querySelector('picture');
   parentWrapper.append(imageContainer);
   parentWrapper.append(headingContainer);
-  // block.querySelectorAll('img').forEach((img) => {
-  //   const picture = img.closest('picture');
-  //   const pTag = picture.closest('p');
-  //   pTag.classList.add('cards-card-image');
-  //   const cardImage = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-  //   const imgTag = cardImage.querySelector('img');
-  //   imgTag.classList.add('max-[799px]:w-full');
-  //   if (picture) picture.replaceWith(cardImage);
-  // });
+  imageContainer.append(pictureTag);
+  block.querySelectorAll('img').forEach((img) => {
+    const picture = img.closest('picture');
+    const cardImage = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    const imgTag = cardImage.querySelector('img');
+    imgTag.classList.add('max-[799px]:w-full');
+    if (picture) picture.replaceWith(cardImage);
+  });
+  const cardHeading = block.querySelector('h3');
+  cardHeading.classList.add(...'card-heading text-2xl tracking-[-0.03em]'.split(' '));
+
+  const cardDescription = block.querySelector('p');
+  if (cardDescription) {
+    cardDescription.classList.add(...'card-description h-full mt-2.5 mb-3 text-base tracking-wide'.split(' '));
+  }
+
+  const cardLink = block.querySelector('p a');
+  if (cardLink) {
+    cardLink.classList.add(...'card-link w-fit text-sm text-white bg-[#2A5F65] hover:bg-[#255159] py-2.5 px-5 rounded-[28px]'.split(' '));
+  } 
+  headingContainer.append(cardHeading);
+  headingContainer.append(cardDescription);
+  headingContainer.append(cardLink);
+
   // [...block.children].forEach((row, index) => {
   //   if (index === 0) {
   //     const heading = row.querySelector('h2');
