3c9f7fe6c365ece76c952ed7b862cfeec33f2eda
Shivangi Singh <shivangi.singh@gspann.com>
2024-08-19 16:12:41 +0530

    Changes

--- blocks/alternating/alternating.js
+++ blocks/alternating/alternating.js
@@ -1,12 +1,8 @@
 export default function decorate(block) {
   block.classList.add(...'lg:flex bg-neutral-100 sm:block mx-auto bg-grey-5 w-[87%] max-[768px]:w-full'.split(' '));
   const innerWrapper = document.querySelector('.alternating');
-  console.log(innerWrapper);
-  //innerWrapper.classList.add(...''.split(' '));
-  const buttonContainer = block.querySelector('.button-container');
   innerWrapper.querySelectorAll('div').forEach((row, index) => {
     row.classList.add('basis-1/2');
-    //row.append(buttonContainer);
     if (index === 0) {
       row.classList.add(...'self-center px-10 py-16 lg:pb-15 md:pl-[62px]'.split(' '));
       row.querySelector('h2')?.classList.add(...'title mb-6 text-4xl font-bold text-heading-large font-header'.split(' '));
