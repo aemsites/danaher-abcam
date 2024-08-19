066595315093aa6b3d217230fb9acdefd5f3c75d
rgravitvl <ravitvl83@gmail.com>
2024-08-19 11:32:40 +0530

    Updated home page blocks

--- blocks/mini-teasers/mini-teasers.js
+++ blocks/mini-teasers/mini-teasers.js
@@ -3,9 +3,9 @@ import { div } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
   block.classList.add(...'w-full bg-black'.split(' '));
-  const divEl = div({class: "flex flex-row mx-auto py-20 w-3/4 space-x-8"});
+  const divEl = div({class: "flex flex-col lg:flex-row mx-auto py-12 xl:w-3/4 space-x-0 lg:space-x-8"});
   [...block.children].forEach((teaser) => {
-      teaser.classList.add(...'text-white w-3/4 px-8'.split(' '));
+      teaser.classList.add(...'text-white w-full lg:w-3/4 py-8 px-8'.split(' '));
       teaser.querySelectorAll('img').forEach((img) => {
         img.closest('picture').replaceWith(createOptimizedPicture(img.src, 'img-alt', false, [{ width: '750' }]));
       });
