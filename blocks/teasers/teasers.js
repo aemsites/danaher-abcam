4fe3e64f46fcaa0234c74bd2a229b4ebc3fddc04
rgravitvl <ravitvl83@gmail.com>
2024-08-20 22:48:37 +0530

    updated the teaser

--- blocks/teasers/teasers.js
+++ blocks/teasers/teasers.js
@@ -29,7 +29,7 @@ function createTeaserBlock(
     ),
     div({ class: 'p-3 rounded-full absolute right-12 bg-teal-700/80 group-hover:bg-teal-700 max-[767px]:right-8' }, openCloseIcon),
   );
-  divEl.append('', summaryContent, '');
+  divEl.append(summaryContent);
   return divEl;
 }
 /**
@@ -44,28 +44,31 @@ export default async function decorate(block) {
     let openCloseIcon;
     let prefixedIcon;
     el.classList.add(...'p-4 mt-1 border rounded-2xl box-border text-black-0 max-w-screen-lg'.split(' '));
+    el.classList.add(...'teaser-item group relative bg-gray-400/10 rounded-2xl box-border text-black-0 max-w-screen-lg [&:has(:focus-visible)]:shadow-interactiveElement cursor-pointer'.split(' '));
+    el.append(span({ class: `icon icon-chevron-down-white flex size-7 absolute left-8 fill-current text-gray-400` }))
+    decorateIcons(el);
     // LOOP the first children of teaser to extract elements
     // like title, subtitle, prefixed-svgs, open-close-svgs
-    [...el.children[0].children].forEach((elChildren) => {
-      if (elChildren.querySelector('strong > u') || elChildren.querySelector('strong > a')) {
-        title = elChildren.querySelector('strong > u')?.innerHTML ?? elChildren.querySelector('strong > a')?.innerHTML;
-        isLink = elChildren.querySelector('strong > a')?.href;
-      } else if (elChildren.querySelector('u')?.innerHTML) {
-        subTitle = elChildren.querySelector('u')?.innerHTML;
-      } else if (elChildren.innerHTML.includes('svg')) {
-        let svgImage = elChildren.innerHTML.replaceAll('&lt;', '<').replaceAll('&gt;', '>');
-        svgImage = new DOMParser().parseFromString(svgImage, 'text/html');
-        svgImage = svgImage.querySelector('svg');
-        const svgClasses = svgImage.classList.value.split('|').join(' ');
-        svgImage.classList.value = '';
-        svgImage.classList.add(...svgClasses.split(' '));
-        if (svgImage.classList.contains('prefixed-title-icon')) {
-          prefixedIcon = svgImage.classList.value;
-        } else if (svgImage.classList.contains('open-close-icon')) {
-          openCloseIcon = svgImage.classList.value;
-        }
-      }
-    });
+    // [...el.children[0].children].forEach((elChildren) => {
+    //   if (elChildren.querySelector('strong > u') || elChildren.querySelector('strong > a')) {
+    //     title = elChildren.querySelector('strong > u')?.innerHTML ?? elChildren.querySelector('strong > a')?.innerHTML;
+    //     isLink = elChildren.querySelector('strong > a')?.href;
+    //   } else if (elChildren.querySelector('u')?.innerHTML) {
+    //     subTitle = elChildren.querySelector('u')?.innerHTML;
+    //   } else if (elChildren.innerHTML.includes('svg')) {
+    //     let svgImage = elChildren.innerHTML.replaceAll('&lt;', '<').replaceAll('&gt;', '>');
+    //     svgImage = new DOMParser().parseFromString(svgImage, 'text/html');
+    //     svgImage = svgImage.querySelector('svg');
+    //     const svgClasses = svgImage.classList.value.split('|').join(' ');
+    //     svgImage.classList.value = '';
+    //     svgImage.classList.add(...svgClasses.split(' '));
+    //     if (svgImage.classList.contains('prefixed-title-icon')) {
+    //       prefixedIcon = svgImage.classList.value;
+    //     } else if (svgImage.classList.contains('open-close-icon')) {
+    //       openCloseIcon = svgImage.classList.value;
+    //     }
+    //   }
+    // });
     return createTeaserBlock(
       { title, subTitle },
       { prefixedIconClass: prefixedIcon, openCloseIconClass: openCloseIcon },
@@ -76,5 +79,4 @@ export default async function decorate(block) {
   });
   block.classList.add(...'space-y-6'.split(' '));
   block.append(...teaserItems);
-  decorateIcons(block);
 }
