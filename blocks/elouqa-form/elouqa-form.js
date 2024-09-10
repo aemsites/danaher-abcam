78754abafaaade1d36b3ff5d13ef947cc58da8b2
Amlan Sengupta <amlans3@gmail.com>
2024-09-11 00:10:11 +0530

    Rectified issue 3

--- blocks/elouqa-form/elouqa-form.js
+++ blocks/elouqa-form/elouqa-form.js
@@ -3,7 +3,7 @@ import {
 } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
-  console.log(block);
+  // console.log(block);
   const formEl = form();
   [...block.children].forEach((child, childIndex) => {
     if (childIndex === 0) {
@@ -12,25 +12,27 @@ export default function decorate(block) {
       formEl.name = child?.children[0]?.children[2]?.innerText;
       formEl.action = child?.children[0]?.children[3]?.innerText;
       block.innerHTML = '';
-    } else {
-      formEl.append(
-        div(
-          { class: 'form-group flex flex-col gap-1' },
-          label(
-            {
-              class: '',
-              for: child?.children[0]?.children[2]?.innerText,
-            },
-            child?.children[0]?.children[0]?.innerText,
-          ),
-          input({
-            class: 'p-1 border rounded',
-            id: child?.children[0]?.children[2]?.innerText,
-            name: child?.children[0]?.children[2]?.innerText,
-            type: child?.children[0]?.children[1]?.innerText,
-          }),
+    } else if (child.children.length > 0) {
+      const formInputElLabel = child.children[0].children[0]?.innerText;
+      const formInputElType = child.children[0].children[1]?.innerText;
+      const formInputElName = child.children[0].children[2]?.innerText;
+      const formInputEl = div(
+        { class: 'form-group flex flex-col gap-1' },
+        label(
+          {
+            class: '',
+            for: formInputElName ? formInputElName : formInputElLabel,
+          },
+          formInputElLabel ? formInputElLabel : formInputElName,
         ),
+        input({
+          class: 'p-1 border rounded',
+          id: formInputElName ? formInputElName : formInputElLabel,
+          name: formInputElName ? formInputElName : formInputElLabel,
+          type: formInputElType,
+        }),
       );
+      formEl.append(formInputEl);
     }
     block.append(formEl);
   });
