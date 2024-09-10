53ef27c5f6286b9e5b0a0e5f238168f41e648469
Amlan Sengupta <amlans3@gmail.com>
2024-09-10 23:28:02 +0530

    Rectified issue 2

--- blocks/elouqa-form/elouqa-form.js
+++ blocks/elouqa-form/elouqa-form.js
@@ -1,36 +1,37 @@
-// import {
-//   form, input, label, div,
-// } from '../../scripts/dom-builder.js';
+import {
+  form, input, label, div,
+} from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
   console.log(block);
-  // const formEl = form();
-  // [...block.children].forEach((child, childIndex) => {
-  //   if (childIndex === 0) {
-  //     formEl.method = child.children[0].children[0]?.innerText;
-  //     formEl.id = child.children[0].children[1]?.innerText;
-  //     formEl.name = child.children[0].children[2]?.innerText;
-  //     block.innerHTML = '';
-  //   } else {
-  //     formEl.append(
-  //       div(
-  //         { class: 'form-group flex flex-col gap-1' },
-  //         label(
-  //           {
-  //             class: '',
-  //             for: child.children[0].children[2]?.innerText,
-  //           },
-  //           child.children[0].children[0]?.innerText,
-  //         ),
-  //         input({
-  //           class: 'p-1 border rounded',
-  //           id: child.children[0].children[2]?.innerText,
-  //           name: child.children[0].children[2]?.innerText,
-  //           type: child.children[0].children[1]?.innerText,
-  //         }),
-  //       ),
-  //     );
-  //   }
-  //   block.append(formEl);
-  // });
+  const formEl = form();
+  [...block.children].forEach((child, childIndex) => {
+    if (childIndex === 0) {
+      formEl.method = child?.children[0]?.children[0]?.innerText;
+      formEl.id = child?.children[0]?.children[1]?.innerText;
+      formEl.name = child?.children[0]?.children[2]?.innerText;
+      formEl.action = child?.children[0]?.children[3]?.innerText;
+      block.innerHTML = '';
+    } else {
+      formEl.append(
+        div(
+          { class: 'form-group flex flex-col gap-1' },
+          label(
+            {
+              class: '',
+              for: child?.children[0]?.children[2]?.innerText,
+            },
+            child?.children[0]?.children[0]?.innerText,
+          ),
+          input({
+            class: 'p-1 border rounded',
+            id: child?.children[0]?.children[2]?.innerText,
+            name: child?.children[0]?.children[2]?.innerText,
+            type: child?.children[0]?.children[1]?.innerText,
+          }),
+        ),
+      );
+    }
+    block.append(formEl);
+  });
 }
