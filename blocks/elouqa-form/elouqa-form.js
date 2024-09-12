aa1857954bddb1bd0ce63f82568022466ffe7269
Amlan Sengupta <amlans3@gmail.com>
2024-09-12 13:01:53 +0530

    Rectified Issue 14

--- blocks/elouqa-form/elouqa-form.js
+++ blocks/elouqa-form/elouqa-form.js
@@ -4,8 +4,8 @@ import {
 } from '../../scripts/dom-builder.js';
 
 function detectFormElementType(
-  formInputElType, formInputElName,
-  formInputElLabel, formInputElValue = ""
+  formInputElType, formInputElName, formInputElLabel,
+  formInputElValue = "", formInputElValidations = "",
 ) {
   let typeOfFormElement;
   switch (formInputElType) {
@@ -36,33 +36,33 @@ export default function decorate(block) {
   const formEl = form();
   [...block.children].forEach((child, childIndex) => {
     if (childIndex === 0) {
-      // console.log(child?.children[0]);
       formEl.method = child?.children[0]?.children[0]?.innerText;
       formEl.id = child?.children[0]?.children[1]?.innerText;
       formEl.name = child?.children[0]?.children[2]?.innerText;
       formEl.action = child?.children[0]?.children[3]?.innerText;
-      // child.outerHTML = '';
+      child.outerHTML = '';
     } else if (child.children.length > 0) {
       const formInputElLabel = child.children[0].children[0]?.innerText;
       const formInputElType = child.children[0].children[1]?.innerText;
       const formInputElName = child.children[0].children[2]?.innerText;
-      // const formInputElValue = child.children[0].children[3]?.innerText;
-      // const typeOfFormElement = detectFormElementType(formInputElType, formInputElName, formInputElLabel, "");
-      // const formInputEl = div(
-      //   { class: 'form-group flex flex-col gap-1' },
-      //   label(
-      //     {
-      //       class: 'font-sans tracking-wide font-semibold',
-      //       for: formInputElName ? formInputElName : formInputElLabel,
-      //     },
-      //     formInputElLabel ? formInputElLabel : formInputElName,
-      //   ),
-      //   typeOfFormElement,
-      // );
-      // if (formInputElLabel && formInputElType && formInputElName) {
-      //   // child.outerHTML = '';
-      //   formEl.append(formInputEl);
-      // }
+      const formInputElValidations = child.children[0].children[3]?.innerText;
+      const formInputElValue = child.children[0].children[4]?.innerText;
+      if (formInputElLabel && formInputElType && formInputElName) {
+        const typeOfFormElement = detectFormElementType(formInputElType, formInputElName, formInputElLabel, formInputElValue, formInputElValidations);
+        const formInputEl = formInputElType !== 'hidden' ? div(
+          { class: 'form-group flex flex-col gap-1' },
+          label(
+            {
+              class: 'font-sans tracking-wide font-semibold',
+              for: formInputElName ? formInputElName : formInputElLabel,
+            },
+            formInputElLabel ? formInputElLabel : formInputElName,
+          ),
+          typeOfFormElement,
+        ) : typeOfFormElement;
+        child.outerHTML = '';
+        formEl.append(formInputEl);
+      }
     }
   });
   if (formEl && formEl.children.length > 0) {
