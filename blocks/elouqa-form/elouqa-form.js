ed311b536a6a8b98fd95e1e0c340a134e5496274
Amlan Sengupta <amlans3@gmail.com>
2024-09-18 00:07:16 +0530

    elouqa changes

--- blocks/elouqa-form/elouqa-form.js
+++ blocks/elouqa-form/elouqa-form.js
@@ -8,7 +8,6 @@ function handleValidations(validations, element) {
   let message = '';
   const allValidations = validations.split('|');
   if (allValidations.includes('required')) {
-    console.log(element, element.parentElement);
     if (element.value.trim() === '') {
       isError = true;
       message = 'Field is required';
@@ -26,34 +25,26 @@ function handleValidations(validations, element) {
   }
 }
 
-function detectFormElementType(
-  formInputElType, formInputElName, formInputElId,
-  formInputElValue = '', formInputElOptions = '', formInputElDataValue = '',
-) {
+function detectFormElementType(formInputElType, formInputElName, formInputElId, formInputElValue = '', formInputElOptions = '', formInputElDataValue = '') {
   let typeOfFormElement;
   switch (formInputElType) {
-    case "options":
-      const allOptions = formInputElOptions.trim() !== ''
-        ? formInputElOptions.split('|').map((opt) => {
-          const newOption = opt.split('->');
-          return option({ value: newOption[0] }, newOption[1]);
-        })
-        : '';
-      typeOfFormElement = select(
-        {
-          class: 'p-1 border rounded',
-          id: formInputElId ? formInputElId : formInputElName,
-          name: formInputElName ? formInputElName : formInputElId,
-          'data-value': formInputElDataValue,
-        },
-        ...allOptions
-      );
+    case 'options':
+      const allOptions = formInputElOptions.split('|').map((opt) => {
+        const newOption = opt.split('->');
+        return option({ value: newOption[0] }, newOption[1]);
+      });
+      typeOfFormElement = select({
+        class: 'p-1 border rounded',
+        id: formInputElId ?? formInputElName,
+        name: formInputElName ?? formInputElId,
+        'data-value': formInputElDataValue,
+      }, ...allOptions);
       break;
     default:
       typeOfFormElement = input({
         class: `p-1 border rounded-md ${formInputElType === 'submit' ? 'text-white bg-green-400' : ''}`,
-        id: formInputElId ? formInputElId : formInputElName,
-        name: formInputElName ? formInputElName : formInputElId,
+        id: formInputElId ?? formInputElName,
+        name: formInputElName ?? formInputElId,
         type: formInputElType,
         value: formInputElValue,
         'data-value': formInputElDataValue,
@@ -64,7 +55,6 @@ function detectFormElementType(
 }
 
 export default function decorate(block) {
-  // console.log(block);
   const formEl = form({ class: 'flex flex-col gap-4' });
   [...block.children].forEach((child, childIndex) => {
     const firstElementChildren = child.children[0].children;
@@ -74,6 +64,13 @@ export default function decorate(block) {
       formEl.name = firstElementChildren[2]?.innerText;
       formEl.action = firstElementChildren[3]?.innerText;
       child.outerHTML = '';
+    } else if (firstElementChildren.length === 1) {
+      child.classList.add('hidden');
+      formEl.append(input({
+        class: 'text-white bg-green-400 p-1 border rounded-md',
+        value: firstElementChildren[0]?.innerText,
+        type: 'submit',
+      }));
     } else if (child.children.length > 0) {
       const formInputElLabel = firstElementChildren[0]?.innerText;
       const formInputElType = firstElementChildren[1]?.innerText;
@@ -84,28 +81,24 @@ export default function decorate(block) {
       const formInputElDataValue = firstElementChildren[6]?.innerText;
       const formInputElId = firstElementChildren[7]?.innerText;
       if (formInputElLabel && formInputElType && formInputElName) {
-        const typeOfFormElement = detectFormElementType(
-          formInputElType, formInputElName, formInputElId,
-          formInputElValue, formInputElOptions, formInputElDataValue,
-        );
+        // eslint-disable max-len
+        const typeOfFormElement = detectFormElementType(formInputElType, formInputElName, formInputElId, formInputElValue, formInputElOptions, formInputElDataValue);
         if (formInputElValidations !== '' && typeOfFormElement) {
           typeOfFormElement.addEventListener(
             'blur',
             () => handleValidations(formInputElValidations, typeOfFormElement),
           );
         }
-        const formInputEl = formInputElType !== 'hidden' ? div(
+        const formInputEl = !['hidden', 'submit'].includes(formInputElType) ? div(
           { class: 'form-group flex flex-col gap-1' },
-          label(
-            {
-              class: 'font-sans tracking-wide font-semibold',
-              for: formInputElName ? formInputElName : formInputElLabel,
-            },
-            formInputElLabel ? formInputElLabel : formInputElName,
-          ),
+          label({
+            class: 'font-sans tracking-wide font-semibold',
+            for: formInputElName ?? formInputElLabel,
+          }, formInputElLabel),
           typeOfFormElement,
         ) : typeOfFormElement;
         // child.outerHTML = '';
+        child.classList.add('hidden');
         formEl.append(formInputEl);
       }
     }
