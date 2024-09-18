92d19c045b0da1edbd5e74403ba61d8d1278aaac
Pardeep Gera <pardeep.gera@gspann.com>
2024-09-18 15:05:43 +0530

    added gtm script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,4 +4,39 @@ import { sampleRUM } from './aem.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+// google tag manager -start
+function loadGTM() {
+    const scriptTag = document.createElement('script');
+    scriptTag.innerHTML = `
+          let gtmId = 'GTM-PDRV95V’';
+          // googleTagManager
+          (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({
+                  'gtm.start':
+                      new Date().getTime(), event: 'gtm.js'
+              });
+              var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src =
+                  'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', gtmId);
+          `;
+    document.head.prepend(scriptTag);
+    const noScriptTag = document.createElement('noscript');
+    noScriptTag.innerHTML = `
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDRV95V"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `;
+    document.body.prepend(noScriptTag);
+  }
+  // google tag manager -end
+  
+  if (
+    !window.location.hostname.includes('localhost')
+      && !window.location.hostname.includes('.hlx')
+  ) {
+    loadGTM();
+  }
