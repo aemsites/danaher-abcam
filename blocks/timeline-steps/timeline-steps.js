eca95acff6264d3b51cb4e04bbd328f9f66c5de0
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-09-20 14:54:06 +0530

    updated timeline-steps related code

--- blocks/timeline-steps/timeline-steps.js
+++ blocks/timeline-steps/timeline-steps.js
@@ -18,9 +18,9 @@ function detectNextElements(stepsOl) {
 export default async function decorate(block) {
   const timeline = [...block.children].map((element) => {
     const timelineWrapper = div();
-    const title = element.querySelector('h2');
-    title.className = 'text-3xl mb-6 font-semibold text-heading-large font-header md:pt-20 md:-mt-20';
-    timelineWrapper.append(title);
+   // const title = element.querySelector('h2');
+   // title.className = 'text-3xl mb-6 font-semibold text-heading-large font-header md:pt-20 md:-mt-20';
+   // timelineWrapper.append(title);
     const description = document.createElement('sub');
     const subtitle = element.children[1]?.querySelector('p');
     const subtitleFootnote = element.children[1]?.querySelector('ul');
