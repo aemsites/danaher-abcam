1eea5d0512e55ceeca40ca5472bf2b17f26310ba
rgravitvl <ravitvl83@gmail.com>
2024-09-20 18:20:05 +0530

    fixed lint issues

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,12 +2,12 @@ import { createOptimizedPicture, getMetadata } from '../../scripts/aem.js';
 import { div } from '../../scripts/dom-builder.js';
 
 const widthRatios = [
-  '2-col-width-1-2', 
+  '2-col-width-1-2',
   '2-col-width-3-4',
   '2-col-width-1-4',
   '2-col-width-3-5',
-  '2-col-width-2-5'
-]
+  '2-col-width-2-5',
+];
 
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
@@ -15,7 +15,7 @@ export default function decorate(block) {
   const imageAspectRatio = 1.7778;
 
   const applyClasses = (element, classes) => element.classList.add(...classes.split(' '));
-  
+
   applyClasses(block, 'h-full lg:max-h-[475px] flex flex-col md:flex-row gap-y-6 px-6 md:px-0');
 
   [...block.children].forEach((row) => {
@@ -38,8 +38,7 @@ export default function decorate(block) {
     });
 
     [...row.children].forEach((col, colIndex) => {
-
-      if(colIndex !== 0) col.classList.add('lg:w-1/2');
+      if (colIndex !== 0) col.classList.add('lg:w-1/2');
 
       col.classList.add('py-6', 'pr-6', block.classList.contains('text-center-align') && !col.querySelector('iframe') ? 'my-auto' : 'h-full');
 
@@ -47,11 +46,11 @@ export default function decorate(block) {
       if (pic) {
         const img = pic.querySelector('img');
         createOptimizedPicture(img.src, 'img-alt', false, [{ width: '750' }]);
-        
+
         if (img && block.classList.contains('image-full-width')) {
           applyClasses(img, 'relative lg:absolute w-full lg:w-1/2 h-full object-cover lg:right-0 lg:bottom-6');
           img.onerror = () => {
-            img.width = img.width;
+            img.width = this.width;
             img.height = Math.floor(img.width / imageAspectRatio);
           };
         }
