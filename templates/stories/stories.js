c08bc9d60e8296c0770fa68d8de2c3f3ae8657bd
wz914876 <sharanagouda_patil@pall.com>
2024-09-20 15:33:01 +0530

    updated sidelinks

--- templates/stories/stories.js
+++ templates/stories/stories.js
@@ -1,4 +1,5 @@
 import { buildBlock, getMetadata } from '../../scripts/aem.js';
+import { div, span } from '../../scripts/dom-builder.js';
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
@@ -8,10 +9,18 @@ export default async function buildAutoBlocks() {
   sectionColumns.prepend(
     buildBlock('back-navigation', { elems: [] }),
   );
-
+  
   sectionMiddle.classList.add(...'story-middle-container w-full'.split(' '));
+  const sideLinksDiv = div({ class: 'sidelinks leading-5 text-sm font-bold text-black pb-4' }, 'Explore Our Products');
+  main.querySelectorAll('p')?.forEach((paragraph) => {
+    if (paragraph.querySelector('a[title="link"]')) {
+      paragraph.classList.add(...'border-b border-b-gray-300 py-2 mx-0 w-auto mt-2'.split(' '));
+      sideLinksDiv.append(span({ class: 'leading-5 text-normal font-medium text-[#378189]' }, paragraph));
+    }
+  });
   sectionMiddle.prepend(
     buildBlock('story-info', { elems: [] }),
     buildBlock('social-media', { elems: [] }),
-  );
+    sideLinksDiv,
+  ); 
 }
