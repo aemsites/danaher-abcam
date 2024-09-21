96144a90b85d1529f07d4c9519110e5d7cb1da40
Amlan Sengupta <amlans3@gmail.com>
2024-09-22 00:28:31 +0530

    filter params DOM structure implemented

--- blocks/filters/filters.js
+++ blocks/filters/filters.js
@@ -1,3 +1,5 @@
+import { decorateIcons } from '../../scripts/aem.js';
+import { div, input, label, li, span, ul } from '../../scripts/dom-builder.js';
 import ffetch from '../../scripts/ffetch.js';
 
 export default async function decorate(block) {
@@ -24,11 +26,32 @@ export default async function decorate(block) {
         if (
           topic[num] !== ''
           && !filterCategory[num].includes(topic[num])
-        ) {
-          filterCategory[num].push(topic[num]);
-        }
+        ) filterCategory[num].push(topic[num]);
       });
     }
-    console.log(jsonName, response, filterCategory);
+    console.log(jsonName, filterNames, response, filterCategory);
+    for (let categoryIndex = 0; categoryIndex < Object.keys(filterCategory).length; categoryIndex += 1) {
+      const categoryKeyName = Object.keys(filterCategory)[categoryIndex];
+      console.log(categoryKeyName);
+      const lists = ul({ class: 'space-y-2' });
+      [...filterCategory[categoryKeyName]].map((categories) => {
+        lists.append(li(
+          { class: 'flex items-center gap-3' },
+          input({ class: 'accent-teal-800 hover:accent-teal-600', type: 'checkbox', name: [categoryKeyName], id: `${[categoryKeyName]}-${categories}` }),
+          label({ class: 'text-sm font-medium capitalize', for: `${[categoryKeyName]}-${categories}` }, categories),
+        ));
+      });
+      const accordionSection = div(
+        { class: 'px-6 py-4 border border-gray-300 rounded-xl' },
+        div(
+          { class: 'flex items-center justify-between mb-2' },
+          span({ class: 'text-base font-bold capitalize' }, categoryKeyName),
+          span({ class: 'icon icon-chevron-down size-5' })
+        ),
+        lists,
+      );
+      block.append(accordionSection);
+    }
+    decorateIcons(block);
   }
 }
