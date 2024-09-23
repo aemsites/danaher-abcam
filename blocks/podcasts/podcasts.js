2dbd12cda97371a9b8391bec11499cc89df2250e
rgravitvl <ravitvl83@gmail.com>
2024-09-23 20:30:57 +0530

    Updated for mobile view

--- blocks/podcasts/podcasts.js
+++ blocks/podcasts/podcasts.js
@@ -1,6 +1,6 @@
 import { decorateIcons } from '../../scripts/aem.js';
 import { applyClasses } from '../../scripts/scripts.js';
-import { div, span } from '../../scripts/dom-builder.js';
+import { a, div, span } from '../../scripts/dom-builder.js';
 
 function decoratePodcast(podcast) {
   applyClasses(podcast, 'podcast-list flex lg:flex-row border-[#D8D8D8] border-b-2 py-8 relative flex-col');
@@ -13,7 +13,7 @@ function decoratePodcast(podcast) {
   const contentDiv = podcast.querySelector('div:nth-child(2)');
   applyClasses(contentDiv, 'lg:ml-6 lg:w-2/3 xl:w-3/4 w-full ml-0');
 
-  const allButtons = div({class: 'flex flex-row gap-6'});
+  const allButtons = div({class: 'grid grid-cols-2 gap-6 lg:w-7/12 w-full'});
 
   const iconContainer = div({ class: 'flex flex-col lg:flex-row mt-2 lg:gap-4 justify-between' }, 
     div('Also Available on'), 
@@ -26,14 +26,21 @@ function decoratePodcast(podcast) {
   });
   decorateIcons(iconContainer);
 
-  const h3El = contentDiv.querySelector('h3');
   const button = podcast.querySelector('div.button-container');
+  button.classList.add('h-10', 'mt-2');
   button.querySelector('p')?.classList.add('!my-0');
   allButtons.append(button, iconContainer);
   contentDiv.append(allButtons);
 
+  const h2El = contentDiv.querySelector('h2');
+  const audioDiv = podcast.querySelector('div:nth-child(3)');
+  applyClasses(audioDiv, 'right-[-7rem] lg:right-0 top-[-1rem] lg:top-0 relative lg:absolute');
+  const headContainer = div({ class: 'grid grid-cols-2' }, h2El, audioDiv);
+  contentDiv.prepend(headContainer);
+
   const audioPlayIconContainers = podcast.querySelector('.audio-play-icon');
   audioPlayIconContainers.addEventListener('click', () => {
+    const h3El = contentDiv.querySelector('h3');
     const audioPlayer = audioPlayIconContainers.parentElement.querySelector('.audio-player');
     h3El.after(audioPlayer);
   });
