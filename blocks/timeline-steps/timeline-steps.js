8e7330223fd0d1b99e5468b19fb56568582464bc
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-09-25 18:12:57 +0530

    modified files for stepline

--- blocks/timeline-steps/timeline-steps.js
+++ blocks/timeline-steps/timeline-steps.js
@@ -1,15 +1,7 @@
-import { decorateIcons } from '../../scripts/aem.js';
-import { div, h5, span } from '../../scripts/dom-builder.js';
+// import { decorateIcons } from '../../scripts/aem.js';
+import { div } from '../../scripts/dom-builder.js';
 
-let nextElement = [];
-
-function detectNextElements(stepsOl) {
-  if (stepsOl.nextElementSibling && ['p', 'ul'].includes(stepsOl.nextElementSibling.localName)) {
-    nextElement.push(stepsOl.nextElementSibling);
-    return detectNextElements(stepsOl.nextElementSibling);
-  }
-  return false;
-}
+// const nextElement = [];
 
 /**
  * loads and decorates the header, mainly the nav
@@ -21,6 +13,5 @@ export default async function decorate(block) {
   const timelineWrapper = div(); let title = block.querySelector('h2');
   title = title != null ? title : 'Default Title';
   title.className = 'text-3xl mb-6 font-semibold text-heading-large font-header md:pt-20 md:-mt-20';
-  timelineWrapper.append(title); 
-    
+  timelineWrapper.append(title);
 }
