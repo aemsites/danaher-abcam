7af5e5dbdefa24d22f5ca1f0e37c451ad7754e8e
venkateshamkeerthi <166115333+venkateshamkeerthi@users.noreply.github.com>
2024-09-27 18:07:31 +0530

    Update timeline-steps.js

--- blocks/timeline-steps/timeline-steps.js
+++ blocks/timeline-steps/timeline-steps.js
@@ -8,13 +8,13 @@ export default async function decorate(block) {
   const title = block.querySelector('h2');
   title?.classList.add(...'text-3xl mb-6 font-semibold text-heading-large font-header md:pt-20 md:-mt-20'.split(' '));
   [...block.children].map((element) => {
-    const steps = element.querySelectorAll('ol');
+    const steps = element.querySelectorAll('ol list-style-type: number');
     if (steps && steps.length > 0) {
       let index = 1;
       [...steps].map((eachStep) => {
         [...eachStep.children].forEach((step) => {
           step.className = 'flex gap-x-4 mb-6';
-          const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, index);
+          const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full list-decimal' }, index);
           const stepDivider = div({ class: 'border border-gray-100' });
           const stepContent = div({ class: 'flex flex-col gap-y-4 py-2' });
           const subPoints = step.querySelector('ul');
@@ -26,9 +26,9 @@ export default async function decorate(block) {
           step.append(stepContent);
           index += 1;
         });
-        return null;
+        return eachStep;
       });
     }
-    return null;
+    return steps;
   });
 }
