45fb8fec868c762801aa733bf1c1959c59dd6364
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-09-27 18:08:58 +0530

    changed orderlist to unorderlist

--- blocks/timeline-steps/timeline-steps.js
+++ blocks/timeline-steps/timeline-steps.js
@@ -14,7 +14,7 @@ export default async function decorate(block) {
       [...steps].map((eachStep) => {
         [...eachStep.children].forEach((step) => {
           step.className = 'flex gap-x-4 mb-6';
-          const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, index);
+          const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full list-decimal' }, index);
           const stepDivider = div({ class: 'border border-gray-100' });
           const stepContent = div({ class: 'flex flex-col gap-y-4 py-2' });
           const subPoints = step.querySelector('ul');
