fb8cdca0b4d3ed1df4da4eb0b72341a020c5e82d
wz914876 <sharanagouda_patil@pall.com>
2024-09-30 19:53:22 +0530

    updated social links

--- blocks/speakersinfo/speakersinfo.js
+++ blocks/speakersinfo/speakersinfo.js
@@ -3,22 +3,18 @@ import {
 } from '../../scripts/aem.js';
 import { div, span } from '../../scripts/dom-builder.js';
 
-export default function decorate(block) {
-  
-  console.log('block', block);
+export default function decorate(block) {  
   const parentEl = block?.parentElement?.parentElement;
-  const socialLinksDiv = div({ class: 'space-y-2' });
-  parentEl.lastElementChild.querySelectorAll('p').forEach((element) => {
-    element.parentElement.classList.add('flex');
-    // if(element.textContent.includes('Linkedin')) {
-    //   socialLinksDiv.append(span({ class: 'icon icon-linkedIn-circle' }), element);
-    // } 
-    // else {
-    //   socialLinksDiv.append(span({ class: 'icon icon-twitter-circle' }), element);
-    // }
-    console.log('element', element.textContent);
+  const divEl = div({ class: 'flex' });
+  const socialLinks = parentEl.lastElementChild.querySelectorAll('p');
+  parentEl.lastElementChild.remove();
+  socialLinks.forEach((element) => {
+    if(element.textContent.includes('@')) {
+      divEl.append(span({ class: 'icon icon-twitter_s' }), element);
+    } else {
+      divEl.append(span({ class: 'icon icon-linkedin_s' }), element);
+    }
   });
-  decorateIcons(socialLinksDiv);
 
   const pictureEl = block.querySelector('picture');
   pictureEl?.classList.add(...'shrink-0'.split(' '));
@@ -31,19 +27,16 @@ export default function decorate(block) {
   const subTitleEl = block.querySelector('h6');
   subTitleEl?.classList.add(...'text-sm font-normal leading-6 text-[#65797C] w-full m-0'.split(' '));
   const descriptionEl = block.querySelector('p');
-  descriptionEl?.classList.add(...'text-lg font-normal text-[#071112] leading-7 tracking-normal'.split(' '));
+  descriptionEl?.classList.add(...'text-lg font-normal text-[#071112] leading-7 tracking-normal'.split(' '));  
   
   const mainDiv = div({ class: 'flex flex-col' },
     div({ class: 'flex', }, pictureEl, 
     div({ class: 'my-auto'}, titleEl, subTitleEl)),
-    div({ class: 'w-full' }, descriptionEl,),
-    socialLinksDiv
+    div({ class: 'w-full' }, descriptionEl,), 
+    divEl,
   );
 
   block.innerHTML = '';
   block.appendChild(mainDiv);
-  // block.appendChild(socialLinksDiv);
-  // parentEl.lastElementChild.classList.add(...'flex'.split(' '));
-  // console.log('block', parentEl.lastElementChild.querySelectorAll('p'));
   decorateIcons(block);
 }
