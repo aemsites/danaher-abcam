e13a7ff6395cd7c64515377e49d17661b7aadd41
wz914876 <sharanagouda_patil@pall.com>
2024-10-01 11:14:54 +0530

    lint errors fixes

--- eds/templates/webinars/webinars.js
+++ eds/templates/webinars/webinars.js
@@ -1,16 +1,16 @@
 import {
-    buildBlock,
-  } from '../../scripts/aem.js';
-  import {
-    div,
-  } from '../../scripts/dom-builder.js';
-  
-  export default async function buildAutoBlocks() {
-    const main = document.querySelector('main');
-    const sectionColumns = main.querySelector(':scope > div > div.columns')?.parentElement;
-    const titleDiv = div({ class: 'webinartitle leading-6 text-2xl font-bold text-black px-6 lg:px-80 mb-6' }, 'Abcam webinar');
-    sectionColumns.prepend(
-      titleDiv,
-      buildBlock('back-navigation', { elems: [] }),
-    );
-  }
+  buildBlock,
+} from '../../scripts/aem.js';
+import {
+  div,
+} from '../../scripts/dom-builder.js';
+
+export default async function buildAutoBlocks() {
+  const main = document.querySelector('main');
+  const sectionColumns = main.querySelector(':scope > div > div.columns')?.parentElement;
+  const titleDiv = div({ class: 'webinartitle leading-6 text-2xl font-bold text-black px-6 lg:px-80 mb-6' }, 'Abcam webinar');
+  sectionColumns.prepend(
+    titleDiv,
+    buildBlock('back-navigation', { elems: [] }),
+  );
+}
