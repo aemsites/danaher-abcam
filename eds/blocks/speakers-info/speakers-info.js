ba88d4956db0bc6137f8324790f861647589bf98
wz914876 <sharanagouda_patil@pall.com>
2024-10-08 15:22:53 +0530

    updated with applyClasses

--- eds/blocks/speakers-info/speakers-info.js
+++ eds/blocks/speakers-info/speakers-info.js
@@ -4,24 +4,25 @@ import {
 import {
   div, span,
 } from '../../scripts/dom-builder.js';
+import { applyClasses } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const parentBlock = block.parentElement.parentElement.firstElementChild;
   const parentBlockTitle = parentBlock.querySelector('p');
-  parentBlockTitle.classList.add(...'text-black text-2xl font-medium leading-7'.split(' '));
+  applyClasses(parentBlockTitle, 'text-black text-2xl font-medium leading-7');
 
   const pictureEl = block.querySelector('picture');
-  pictureEl?.classList.add(...'shrink-0'.split(' '));
+  applyClasses(pictureEl, 'shrink-0');
   const img = pictureEl?.querySelector('img');
   createOptimizedPicture(img?.src, 'img-alt', false, [{ width: '750' }]);
-  img?.classList.add(...'w-16 h-16 rounded-full'.split(' '));
+  applyClasses(img, 'w-16 h-16 rounded-full');
 
   const titleEl = block.querySelector('h5');
-  titleEl?.classList.add(...'text-normal font-semibold text-[#071112] m-0'.split(' '));
+  applyClasses(titleEl, 'text-normal font-semibold text-[#071112] m-0');
   const subTitleEl = block.querySelector('h6');
-  subTitleEl?.classList.add(...'text-sm font-normal leading-6 text-[#65797C] w-full m-0'.split(' '));
+  applyClasses(subTitleEl, 'text-sm font-normal leading-6 text-[#65797C] w-full m-0');
   const descriptionEl = block.querySelector('p');
-  descriptionEl?.classList.add(...'text-lg font-normal text-[#071112] leading-7 tracking-normal'.split(' '));
+  applyClasses(descriptionEl, 'text-lg font-normal text-[#071112] leading-7 tracking-normal');
 
   const parentEl = block?.parentElement?.parentElement;
   const divEl = div({ class: 'flex text-base font-normal leading-6 text-[#378189] underline gap-x-2' });
