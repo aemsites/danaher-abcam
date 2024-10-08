d73a0ee69fe7157c8ca8a68e27514002bc8bf29e
Rajesh Chithaluri <rajesh.chithaluri@gspann.com>
2024-10-08 18:45:07 +0530

    Added logic to play any one media at a time and some UI issues

--- eds/blocks/podcasts/podcasts.js
+++ eds/blocks/podcasts/podcasts.js
@@ -3,7 +3,7 @@ import { applyClasses } from '../../scripts/scripts.js';
 import { div, span } from '../../scripts/dom-builder.js';
 
 function decoratePodcast(podcast) {
-  applyClasses(podcast, 'podcast-list flex lg:flex-row border-[#D8D8D8] border-b-2 py-8 relative flex-col');
+  applyClasses(podcast, 'podcast-list flex lg:flex-row border-[#D8D8D8] border-b-2 py-8 relative flex-col lg:px-8');
   const pictureDiv = podcast.querySelector('div:nth-child(1)');
   const img = pictureDiv.querySelector('img');
   if (img) {
@@ -13,12 +13,12 @@ function decoratePodcast(podcast) {
   const contentDiv = podcast.querySelector('div:nth-child(2)');
   applyClasses(contentDiv, 'lg:ml-6 lg:w-2/3 xl:w-3/4 w-full ml-0');
 
-  const allButtons = div({ class: 'grid grid-cols-2 gap-6 lg:w-7/12 w-full items-center' });
+  const allButtons = div({ class: 'grid grid-cols-2 gap-6 lg:w-7/12 w-full !items-center' });
 
   const iconContainer = div(
-    { class: 'flex flex-col lg:flex-row lg:gap-4 justify-between' },
+    { class: 'flex flex-col lg:flex-row lg:gap-2 justify-between' },
     div('Also Available on'),
-    div({ class: 'icon-container flex gap-2' }),
+    div({ class: 'icon-container flex gap-4' }),
   );
   contentDiv.querySelectorAll('p > a').forEach((link) => {
     link.parentElement.remove();
@@ -36,11 +36,13 @@ function decoratePodcast(podcast) {
   contentDiv.append(allButtons);
 
   const h2El = contentDiv.querySelector('h2');
+  applyClasses(h2El, '!text-lg lg:!text-2xl m-0');
   const h3El = contentDiv.querySelector('h3');
+  applyClasses(h3El, '!text-lg lg:!text-2xl m-0');
   const audioDiv = podcast.querySelector('div:nth-child(3)');
-  applyClasses(audioDiv, 'right-[-2.25rem] top-[-0.625rem] lg:right-0 lg:top-0 relative lg:absolute');
+  applyClasses(audioDiv, 'right-[-2.25rem] top-[-0.625rem] lg:right-8 lg:top-0 relative lg:absolute');
   const headContainer = div(
-    { class: 'grid grid-cols-4' },
+    { class: 'grid grid-cols-4 mt-6 lg:mt-0' },
     div({ class: 'w-full col-span-3' }, h2El, h3El),
     audioDiv,
   );
