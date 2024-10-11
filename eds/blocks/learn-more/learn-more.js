2b2e62623d015ee4a643eb9c4cd6d9beb6e4d2f8
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-10-11 10:16:31 +0530

    fixed learn-more mobile view issue button should display after description instead of sociallinks

--- eds/blocks/learn-more/learn-more.js
+++ eds/blocks/learn-more/learn-more.js
@@ -11,7 +11,7 @@ export default function decorate(block) {
   block.querySelector('h3')?.classList.add('text-xl', 'font-bold', 'mb-4');
   block.querySelector('div > p:not(div.button-container > p)')?.classList.add('text-base', 'font-normal', 'mb-4');
 
-  const socialLinksDiv = div({ class: 'max-w-56 pt-10 lg:pt-0' });
+  const socialLinksDiv = div({ class: 'max-w-56 pt-10 lg:pt-0 md:pl-8' });
   socialLinksDiv.prepend(
     p(
       { class: 'text-base font-bold' },
@@ -30,10 +30,10 @@ export default function decorate(block) {
     socialLinksDiv.classList.add(...'flex flex-col w-full items-center m-auto'.split(' '));
     decorateIcons(socialLinksDiv, 50, 50);
   } else {
-    if (firstDivEl?.childElementCount === 0 && button) {
+    if (button) {
       firstDivEl?.append(button);
     }
-    block.querySelector('div')?.classList.add(...'flex flex-col md:flex-row md:justify-between pt-0 pb-8'.split(' '));
+    block.querySelector('div')?.classList.add(...'flex flex-col md:flex-row md:justify-between pt-0 pb-8 md:px-8'.split(' '));
     block.querySelector('h3')?.parentElement?.classList.add('max-w-3xl');
     block.querySelector('h3')?.classList.add('text-xl', 'font-bold', 'mb-4');
     block.querySelector('div > p:not(div.button-container > p)')?.classList.add('text-base', 'font-normal', 'mb-4');
