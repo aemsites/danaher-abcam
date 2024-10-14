f03d628f8bca96cee1ecfedea79da78071e8d7ea
wz914876 <sharanagouda_patil@pall.com>
2024-10-14 12:00:02 +0530

    updated the block

--- eds/blocks/download/download.js
+++ eds/blocks/download/download.js
@@ -4,17 +4,19 @@ import {
 import { getMetadata } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log('decorating download block :', block);
-  // const title = getMetadata('og:title');  
-  // const { href } = window.location;
-  // const downloadDiv = div(
-  //   { class: 'flex flex-col bg-[#f2f2f2] rounded-lg p-6 max-w-[544px] mb-10' },
-  //   h6({ class: 'download-title' }, `${title}`),
-  //   a({
-  //     class: 'button w-fit my-4 bg-black hover:bg-[#3B3B3B] h-10 whitespace-nowrap rounded-[28px] py-2.5 px-5 text-white text-sm capitalize',
-  //     href,
-  //   }, 'Download'),
-  //   p({ class: 'text-xs text-black-0' }, 'PDF'),
-  // );
-  // block.append(downloadDiv);
+  const title = getMetadata('og:title');
+  const downloadLink = block.querySelector('p > a');
+  const linkText = downloadLink.textContent;
+  const linkHref = downloadLink.href;
+  const downloadDiv = div(
+    { class: 'flex flex-col bg-[#f2f2f2] rounded-lg p-6 max-w-[544px] mb-10' },
+    h6({ class: 'download-title' }, `${title}`),
+    a({
+      class: 'button w-fit my-4 bg-black hover:bg-[#3B3B3B] h-10 whitespace-nowrap rounded-[28px] py-2.5 px-5 text-white text-sm capitalize',
+      href: linkHref,
+    }, linkText),
+    p({ class: 'text-xs text-black-0' }, 'PDF'),
+  );
+  block.innerHTML = '';
+  block.append(downloadDiv);
 }
