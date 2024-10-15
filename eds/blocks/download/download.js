141e0d9f51c196dfdb69916800093ee8c0e481e1
wz914876 <sharanagouda_patil@pall.com>
2024-10-15 11:58:37 +0530

    added block changes

--- eds/blocks/download/download.js
+++ eds/blocks/download/download.js
@@ -4,17 +4,29 @@ import {
 import { getMetadata } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log('decorate download block: ', block);
-  // const title = getMetadata('og:title');
-  // const { href } = window.location;
-  // const downloadBlock = div(
-  //   { class: 'flex flex-col bg-[#f2f2f2] rounded-lg p-6 max-w-[544px] mb-10' },
-  //   h6({ class: 'download-title' }, `${title}`),
-  //   a({
-  //     class: 'button w-fit my-4 bg-black hover:bg-[#3B3B3B] h-10 whitespace-nowrap rounded-[28px] py-2.5 px-5 text-white text-sm capitalize',
-  //     href,
-  //   }, 'Download'),
-  //   p({ class: 'text-xs text-black-0' }, 'PDF'),
-  // );
-  // block.append(downloadBlock);
+  const ogTitle = getMetadata('og:title');
+  const title = ogTitle.indexOf('| abcam') > -1 ? ogTitle.split('| abcam')[0] : ogTitle;
+  let linkText;
+  let linkHref;
+  let docType;
+  let downloadDiv;
+  block.querySelectorAll('p').forEach((element) => {
+    if (element.querySelector('a')) {
+      linkText = element.textContent;
+      linkHref = element.href;
+    } else {
+      docType = element.textContent;
+    }
+    downloadDiv = div(
+      { class: 'flex flex-col bg-[#f2f2f2] rounded-lg p-6 max-w-[544px] mt-6 mb-2' },
+      h6({ class: 'download-title' }, `${title}`),
+      a({
+        class: 'button w-fit my-4 bg-black hover:bg-[#3B3B3B] h-10 whitespace-nowrap rounded-[28px] py-2.5 px-5 text-white text-sm capitalize',
+        href: linkHref,
+      }, linkText),
+      p({ class: 'text-xs text-black-0' }, docType),
+    );
+  });
+  block.innerHTML = '';
+  block.append(downloadDiv);
 }
