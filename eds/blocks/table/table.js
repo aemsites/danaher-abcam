2384a7183372185669cd9971212eb8ed126db25b
rgravitvl <ravitvl83@gmail.com>
2024-10-15 21:07:05 +0530

    commented out all in table

--- eds/blocks/table/table.js
+++ eds/blocks/table/table.js
@@ -9,32 +9,32 @@ function buildCell(rowIndex) {
 }
 
 export default async function decorate(block) {
-  const mainTable = table({ class: 'table-fixed table-auto w-full' });
-  const mainThead = thead({});
-  const mainTbody = tbody({});
+  // const mainTable = table({ class: 'table-fixed table-auto w-full' });
+  // const mainThead = thead({});
+  // const mainTbody = tbody({});
 
-  const header = !block.classList.contains('no-header');
-  if (header) {
-    mainTable.append(mainThead);
-  }
-  mainTable.append(mainTbody);
+  // const header = !block.classList.contains('no-header');
+  // if (header) {
+  //   mainTable.append(mainThead);
+  // }
+  // mainTable.append(mainTbody);
 
-  [...block.children].forEach((child, i) => {
-    const tableRow = tr({ class: 'border-b-2 border-[#e5e7eb]' });
-    if (header && i === 0) mainThead.append(tableRow);
-    else mainTbody.append(tableRow);
-    [...child.children].forEach((col) => {
-      const cell = buildCell(header ? i : i + 1);
-      tableRow.append(cell);
-      const anchorTd = col.querySelector('a');
-      if (anchorTd) {
-        anchorTd.classList.add(...'text-base text-[#378189] underline'.split(' '));
-        cell.appendChild(anchorTd);
-      } else {
-        cell.innerHTML = col.innerHTML;
-      }
-    });
-  });
-  block.innerHTML = '';
-  block.append(mainTable);
+  // [...block.children].forEach((child, i) => {
+  //   const tableRow = tr({ class: 'border-b-2 border-[#e5e7eb]' });
+  //   if (header && i === 0) mainThead.append(tableRow);
+  //   else mainTbody.append(tableRow);
+  //   [...child.children].forEach((col) => {
+  //     const cell = buildCell(header ? i : i + 1);
+  //     tableRow.append(cell);
+  //     const anchorTd = col.querySelector('a');
+  //     if (anchorTd) {
+  //       anchorTd.classList.add(...'text-base text-[#378189] underline'.split(' '));
+  //       cell.appendChild(anchorTd);
+  //     } else {
+  //       cell.innerHTML = col.innerHTML;
+  //     }
+  //   });
+  // });
+  // block.innerHTML = '';
+  // block.append(mainTable);
 }
