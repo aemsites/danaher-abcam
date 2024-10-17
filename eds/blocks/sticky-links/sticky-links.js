add6d84989a316cbb09a65805e57ec987217000a
Amlan Sengupta <amlans3@gmail.com>
2024-10-17 19:07:41 +0530

    Test 8

--- eds/blocks/sticky-links/sticky-links.js
+++ eds/blocks/sticky-links/sticky-links.js
@@ -2,18 +2,18 @@ import { ul, p, span } from '../../scripts/dom-builder.js';
 import { createCard } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  console.log('story');
   if (block.children.length > 0) {
     const cardList = ul({ class: 'w-full my-3' });
     [...block.children].forEach((article) => {
       if (article.children.length > 0) {
         const obj = {};
+        article.classList.add('hidden');
         if (article.querySelector('p:not(a)')) obj['title'] = article.querySelector('p:not(a)');
-        if (article.querySelector('p a')) obj['path'] = article.querySelector('p a')?.href;
+        if (article.querySelector('p a')) obj['path'] = article.querySelector('p a');
         console.log(obj);
         if (Object.keys(obj).length === 2) {
           cardList.appendChild(createCard({
-            path: obj.path,
+            path: obj.path?.href,
             bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, obj.title),
             isStoryCard: false,
           }));
