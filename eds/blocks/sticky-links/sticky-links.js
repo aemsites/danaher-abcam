ca4801d5a50bad215bf93631440bc1009fee2a86
Amlan Sengupta <amlans3@gmail.com>
2024-10-17 17:15:16 +0530

    test 2

--- eds/blocks/sticky-links/sticky-links.js
+++ eds/blocks/sticky-links/sticky-links.js
@@ -14,19 +14,18 @@ export default async function decorate(block) {
         articles.push(obj);
       }
     });
-  }
-
-  block.innerHTML = '';
-  if (articles.length > 0) {
-    const cardList = ul({ class: 'w-full my-3' });
-    articles.forEach((article) => {
-      cardList.appendChild(createCard({
-        path: article.path,
-        bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, article.title),
-        isStoryCard: false,
-      }));
-    });
-    const spanEl = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C]' }, 'RELATED LINKS');
-    block.append(spanEl, cardList);
+    block.innerHTML = '';
+    if (articles.length > 0) {
+      const cardList = ul({ class: 'w-full my-3' });
+      articles.forEach((article) => {
+        cardList.appendChild(createCard({
+          path: article.path,
+          bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, article.title),
+          isStoryCard: false,
+        }));
+      });
+      const spanEl = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C]' }, 'RELATED LINKS');
+      block.append(spanEl, cardList);
+    }
   }
 }
