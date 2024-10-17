d32dcc15a9a7be65038d60fbf7f71e36143ba0f7
Amlan Sengupta <amlans3@gmail.com>
2024-10-17 16:47:44 +0530

    parsed link-name and link-path

--- eds/blocks/sticky-links/sticky-links.js
+++ eds/blocks/sticky-links/sticky-links.js
@@ -1,23 +1,27 @@
-import ffetch from '../../scripts/ffetch.js';
 import { ul, p, span } from '../../scripts/dom-builder.js';
 import { createCard } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  console.log('Sticky Links');
-  // let articles = await ffetch('/en-us/stories/query-index.json').all();
+  const articles = [];
+  [...block.children].forEach((article) => {
+    const obj = {
+      title: article.querySelector('p:not(a)'),
+      path: article.querySelector('p a')?.href,
+    };
+    articles.push(obj);
+  });
 
-  // block.innerHTML = '';
-  // if (articles.length > 0) {
-  //   articles = articles.sort((item1, item2) => item2.publishDate - item1.publishDate);
-  //   const cardList = ul({ class: 'w-full my-3' });
-  //   articles.forEach((article) => {
-  //     cardList.appendChild(createCard({
-  //       path: article.path,
-  //       bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, article.title),
-  //       isStoryCard: false,
-  //     }));
-  //   });
-  //   const spanEl = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C]' }, 'RELATED LINKS');
-  //   block.append(spanEl, cardList);
-  // }
+  block.innerHTML = '';
+  if (articles.length > 0) {
+    const cardList = ul({ class: 'w-full my-3' });
+    articles.forEach((article) => {
+      cardList.appendChild(createCard({
+        path: article.path,
+        bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, article.title),
+        isStoryCard: false,
+      }));
+    });
+    const spanEl = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C]' }, 'RELATED LINKS');
+    block.append(spanEl, cardList);
+  }
 }
