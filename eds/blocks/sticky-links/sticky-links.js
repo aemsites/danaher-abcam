3b13e53369ed4c29558bc9b1a4f535bd8e21463c
Amlan Sengupta <amlans3@gmail.com>
2024-10-17 18:56:35 +0530

    test 7

--- eds/blocks/sticky-links/sticky-links.js
+++ eds/blocks/sticky-links/sticky-links.js
@@ -3,8 +3,8 @@ import { createCard } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   console.log('story');
-  const articles = [];
   if (block.children.length > 0) {
+    const cardList = ul({ class: 'w-full my-3' });
     [...block.children].forEach((article) => {
       if (article.children.length > 0) {
         const obj = {};
@@ -12,24 +12,15 @@ export default async function decorate(block) {
         if (article.querySelector('p a')) obj['path'] = article.querySelector('p a')?.href;
         console.log(obj);
         if (Object.keys(obj).length === 2) {
-          articles.push(obj);
-          article.remove();
+          cardList.appendChild(createCard({
+            path: obj.path,
+            bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, obj.title),
+            isStoryCard: false,
+          }));
         }
       }
     });
-    console.log(articles);
-    if (articles.length > 0) {
-      const cardList = ul({ class: 'w-full my-3' });
-      articles.forEach((article) => {
-        cardList.appendChild(createCard({
-          path: article.path,
-          bodyEl: span({ class: 'block text-sm leading-6 font-semibold text-[#378189] py-2 border-b border-b-[#D8D8D8] hover:underline' }, article.title),
-          isStoryCard: false,
-        }));
-      });
-      // block.innerHTML = '';
-      const spanEl = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C]' }, 'RELATED LINKS');
-      block.append(spanEl, cardList);
-    }
+    const spanEl = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C]' }, 'RELATED LINKS');
+    block.append(spanEl, cardList);
   }
 }
