672a37f4af68ce68728f295f40d06761a1e17685
Kumar Shoolam <kumar.shoolam@MACPRO572.local>
2024-10-17 18:24:40 +0530

    Changes for Icon issue at Learn More Page || Article Social Icons

--- eds/blocks/learn-more/learn-more.js
+++ eds/blocks/learn-more/learn-more.js
@@ -7,11 +7,11 @@ import { decorateIcons } from '../../scripts/aem.js';
 export default function decorate(block) {
   block.classList.add(...'px-6 py-8 w-full border-y'.split(' '));
   block.querySelector('div')?.classList.add(...'flex flex-col sm:flex-row md:flex-row md:justify-between pt-0 pb-8'.split(' '));
-  block.querySelector('h3')?.parentElement?.classList.add('max-w-3xl');
+  block.querySelector('h3')?.parentElement?.classList.add('md:w-3/4');
   block.querySelector('h3')?.classList.add('text-xl', 'font-bold', 'mb-4');
   block.querySelector('div > p:not(div.button-container > p)')?.classList.add('text-base', 'font-normal', 'mb-4');
 
-  const socialLinksDiv = div({ class: 'max-w-56 pt-10 lg:pt-0 md:pl-8' });
+  const socialLinksDiv = div({ class: 'md:w-1/4 pt-10 lg:pt-0 md:pl-8' });
   socialLinksDiv.prepend(
     p(
       { class: 'text-base font-bold' },
