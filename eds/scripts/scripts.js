2386d7814290596e01d6c1e9f30c5ec320dc5364
Ravi R <ravitvl83@gmail.com>
2024-10-18 11:45:18 +0530

    Added move instrumentation function

--- eds/scripts/scripts.js
+++ eds/scripts/scripts.js
@@ -37,6 +37,21 @@ export function getStoryType(pageTags) {
 }
 
 /**
+ * Move instrumentation attributes from a given element to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveInstrumentation(from, to) {
+  moveAttributes(
+    from,
+    to,
+    [...from.attributes]
+      .map(({ nodeName }) => nodeName)
+      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-')),
+  );
+}
+
+/**
  *
  * @param {*} config
  * @returns response JSON
