53bdf504e370ef80648b13e395ee6a8ef09e7596
Ravi R <ravitvl83@gmail.com>
2024-10-18 11:48:09 +0530

    Updated  moveAttributes

--- eds/scripts/scripts.js
+++ eds/scripts/scripts.js
@@ -37,6 +37,25 @@ export function getStoryType(pageTags) {
 }
 
 /**
+ * Moves all the attributes from a given elmenet to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveAttributes(from, to, attributes) {
+  if (!attributes) {
+    // eslint-disable-next-line no-param-reassign
+    attributes = [...from.attributes].map(({ nodeName }) => nodeName);
+  }
+  attributes.forEach((attr) => {
+    const value = from.getAttribute(attr);
+    if (value) {
+      to?.setAttribute(attr, value);
+      from.removeAttribute(attr);
+    }
+  });
+}
+
+/**
  * Move instrumentation attributes from a given element to another given element.
  * @param {Element} from the element to copy attributes from
  * @param {Element} to the element to copy attributes to
