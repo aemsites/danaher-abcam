f164d29547b69c4134cdf2cd9a0392e0f29c9028
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-10-20 23:25:46 +0530

    updated related-link block in guides template

--- eds/templates/guides/guides.js
+++ eds/templates/guides/guides.js
@@ -9,7 +9,8 @@ export default async function buildAutoBlocks() {
     : main.querySelector(':scope > div:nth-child(3)');
 
   sectionMiddle?.classList.add(...'guides-middle-container w-full'.split(' '));
-  sectionMiddle?.prepend(buildBlock('chapters', { elems: [] }), buildBlock('related-links', { elems: [] }));
+  sectionMiddle?.prepend(buildBlock('related-links', { elems: [] }));
+  sectionMiddle?.prepend(buildBlock('chapters', { elems: [] }));
   const headerSection = div();
   const breadcrumbBlock = buildBlock('breadcrumb', { elems: [] });
   headerSection.append(breadcrumbBlock);
