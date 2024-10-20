5ee3e5b18407182ad7d3d20b7f5472a9a7cb8ece
venkateshamkeerthi <venkatesham.keerthi@gspann.com>
2024-10-20 23:29:51 +0530

    fixed eslint for breadcrumb

--- eds/blocks/breadcrumb/breadcrumb.js
+++ eds/blocks/breadcrumb/breadcrumb.js
@@ -27,7 +27,7 @@ export default function decorate(block) {
       { class: 'breadcrumb-wrapper relative z-10 flex max-w-max flex-1 items-center' },
       div({ style: 'position:relative' }, ul(breadcrumbLiLinks)),
     );
-     block.classList.add(...'px-[30px] m-auto font-sans text-base flex flex-col justify-center'.split(' '));
+    block.classList.add(...'px-[30px] m-auto font-sans text-base flex flex-col justify-center'.split(' '));
     block.appendChild(breadcrumNav);
   }
 }
