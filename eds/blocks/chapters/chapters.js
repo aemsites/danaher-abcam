635c64a693e2503b0e94c7668fd06d493bd181d7
wz914876 <sharanagouda_patil@pall.com>
2024-10-21 15:06:08 +0530

    chapters changes

--- eds/blocks/chapters/chapters.js
+++ eds/blocks/chapters/chapters.js
@@ -1,6 +1,7 @@
 import ffetch from '../../scripts/ffetch.js';
 import { a, div, p } from '../../scripts/dom-builder.js';
 import { makePublicUrl } from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/aem.js';
 
 function renderChapters(chapterItems) {
   const chaptersDiv = div({ class: 'flex flex-col items-start pt-6' });
@@ -24,14 +25,23 @@ function renderChapters(chapterItems) {
 }
 
 export default async function decorate(block) {
-  let chapterItems = [];
-  let chaptersElements = div();
-  chapterItems = await ffetch('/en-us/stories/query-index.json').all();
+  const currentPage = window.location.pathname.split('/').slice(3);
+  const currentPath = currentPage.join('/');
+
+  let chapterItems = await ffetch('/en-us/technical-resources/guides/query-index.json')
+    .filter((item) => {
+      const url = new URL(getMetadata('og:url'));
+      return item.path !== url.pathname;
+    })
+    .filter((item) => item.path.includes(currentPath))
+    .all();
+
   const chapters = chapterItems.map((element) => ({
-    title: element.title, description: element.description, path: element.path,
+    title: element.title.indexOf('| abcam') > -1 ? element.title.split('| abcam')[0] : element.title, 
+    description: element.description, 
+    path: element.path,
   }));
   const filteredChapters = chapters.filter((item) => item.title !== undefined);
   const navHeadingDiv = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C] px-4' }, 'CHAPTERS');
-  chaptersElements = renderChapters(filteredChapters);
-  block.append(navHeadingDiv, chaptersElements);
+  if(chapters.length > 0) block.append(navHeadingDiv, renderChapters(filteredChapters));
 }
