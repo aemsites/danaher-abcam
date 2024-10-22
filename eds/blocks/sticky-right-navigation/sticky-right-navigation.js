0d340b5c3400afaf23ec3dcc5919e51562d7d822
vijayasri palivela <vspalivela@nautilus.com>
2024-10-22 15:19:08 +0530

    lintChecks

--- eds/blocks/sticky-right-navigation/sticky-right-navigation.js
+++ eds/blocks/sticky-right-navigation/sticky-right-navigation.js
@@ -7,10 +7,10 @@ export default function decorate(block) {
   sectionHead.textContent = 'SECTIONS';
   block.appendChild(sectionHead);
   const rightNavLinks = document.querySelectorAll('h2');
-  const navLinksUl = ul({ class: 'flex flex-row flex-wrap sticky-ul mt-4 items-center' }); 
+  const navLinksUl = ul({ class: 'flex flex-row flex-wrap sticky-ul mt-4 items-center' });
   const items = [];
   rightNavLinks.forEach((rightNavLink, index) => {
-    const liElement = li({ class: 'w-50 pr-3.5 pl-3.5 py-2.5 border-r border-gray-200' }); 
+    const liElement = li({ class: 'w-50 pr-3.5 pl-3.5 py-2.5 border-r border-gray-200' });
     const anchorElement = a({ class: 'text-base text-blue-500 block w-full hover:underline' });
     anchorElement.textContent = rightNavLink.textContent;
     anchorElement.href = `#${rightNavLink.id}`;
@@ -25,16 +25,16 @@ export default function decorate(block) {
   });
   const showMoreBtn = document.createElement('div');
   showMoreBtn.textContent = 'See All';
-  showMoreBtn.className = 'cursor-pointer underline mt-2 ml-3'; 
+  showMoreBtn.className = 'cursor-pointer underline mt-2 ml-3';
   let showingAll = false;
   showMoreBtn.addEventListener('click', () => {
     showingAll = !showingAll;
     items.forEach((item, index) => {
       item.style.display = showingAll || index < 4 ? 'block' : 'none';
     });
-    navLinksUl.style.display = 'flex'; 
+    navLinksUl.style.display = 'flex';
     showMoreBtn.textContent = showingAll ? 'Show Less' : 'See All';
   });
-  navLinksUl.appendChild(showMoreBtn); 
+  navLinksUl.appendChild(showMoreBtn);
   block.appendChild(navLinksUl);
 }
