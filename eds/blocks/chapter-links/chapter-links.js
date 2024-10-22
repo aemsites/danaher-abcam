d8e414e8b72244c57636e99b1557c46a2569ef03
wz914876 <sharanagouda_patil@pall.com>
2024-10-22 15:49:04 +0530

    295 Mobile View - chapter links on guide detail page and chapter page

--- eds/blocks/chapter-links/chapter-links.js
+++ eds/blocks/chapter-links/chapter-links.js
@@ -3,22 +3,24 @@ import { a, div, p } from '../../scripts/dom-builder.js';
 import { makePublicUrl } from '../../scripts/scripts.js';
 
 function renderChapters(chapterItems) {
-  const chaptersDiv = div({ class: 'flex flex-col items-start pt-6' });
+  const chaptersDiv = div({ class: 'flex flex-col items-start' });
+  const url = new URL(window.location.href);
+  const currentPage = url.pathname;
   chapterItems.forEach((item) => {
-    chaptersDiv.append(div(
-      {
-        class: 'w-full border-b border-b-[#D8D8D8]',
-      },
-      div(
-        {
-          class: 'flex gap-3',
-        },
-        a({
-          class: 'block text-sm leading-6 font-semibold text-[#378189] p-2 hover:underline',
-          href: makePublicUrl(item.path),
-        }, item.title),
-      ),
-    ));
+    if(item.path === currentPage) {
+      chaptersDiv.append(div({ class: 'w-full border-b border-b-[#D8D8D8]' },
+        div({ class: 'flex gap-3 text-sm leading-6 font-semibold text-black bg-[#EDF6F7] p-2 my-0.5' }, item.title ),
+      ));
+    } else {
+      chaptersDiv.append(div({ class: 'w-full border-b border-b-[#D8D8D8]' },
+        div({ class: 'flex gap-3' },
+          a({
+            class: 'block text-sm leading-6 font-semibold text-[#378189] p-2 hover:underline',
+            href: makePublicUrl(item.path),
+          }, item.title),
+        ),
+      ));
+    }
   });
   return chaptersDiv;
 }
@@ -41,7 +43,9 @@ export default async function decorate(block) {
     description: element.description,
     path: element.path,
   }));
+  
   const filteredChapters = chapters.filter((item) => item.title !== undefined);
+  filteredChapters.sort((chapter1, chapter2) => chapter1.title.localeCompare(chapter2.title));
   const navHeadingDiv = p({ class: 'text-sm leading-6 font-semibold uppercase text-[#65797C] p-2' }, 'CHAPTERS');
   if (chapters.length > 0) block.append(navHeadingDiv, renderChapters(filteredChapters));
 }
