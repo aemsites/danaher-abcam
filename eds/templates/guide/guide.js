9b85b8f38fd1008c4a70575f100aa70557d6d631
rgravitvl <ravitvl83@gmail.com>
2024-10-29 10:15:22 +0530

    Updated sidelinks from fragment

--- eds/templates/guide/guide.js
+++ eds/templates/guide/guide.js
@@ -1,14 +1,34 @@
 import { buildBlock } from '../../scripts/aem.js';
-import { div } from '../../scripts/dom-builder.js';
+import { a, div } from '../../scripts/dom-builder.js';
+
+async function buildRelatedLinks(main) {
+  const allLinks = [];
+  const fragmentBlock = main.querySelector('div.fragment');
+  const fragmentPath = fragmentBlock?.querySelector('a')?.getAttribute('href');
+  fragmentBlock?.remove();
+  const resp = await fetch(`${fragmentPath}.plain.html`);
+  if (resp.ok) {
+    const fragment = document.createElement('div');
+    fragment.innerHTML = await resp.text();
+    const links = fragment.querySelectorAll('a[title="link"]');
+    links.forEach((link) => {
+      const pEl = document.createElement('p');
+      pEl.appendChild(link);
+      allLinks.push(pEl);
+    });
+  }
+  return allLinks;
+}
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
+  const allLinks = await buildRelatedLinks(main);
   const mainEl = main.querySelector('div');
   const sectionMiddle = main.querySelector(':scope > div:nth-child(2)');
   sectionMiddle.classList.add(...'guides-middle-container w-full pt-4'.split(' '));
   sectionMiddle.prepend(
     buildBlock('chapter-links', { elems: [] }),
-    buildBlock('sidelinks', { elems: [] }),
+    buildBlock('sidelinks', { elems: allLinks }),
   );
   sectionMiddle.prepend(
     buildBlock('sticky-sections-list', { elems: [] }),
