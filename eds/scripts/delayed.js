05c96c99de927ba682c3aa84ce3916c9cb66a559
Pardeep Gera <pardeep.gera@gspann.com>
2024-11-01 13:58:43 +0530

    parameterized values

--- eds/scripts/delayed.js
+++ eds/scripts/delayed.js
@@ -73,15 +73,15 @@ function loadrelicScript() {
 
 // freshchat -start
 function loadFreshChat() {
-  let FRESHCHAT_TOKEN = (window.location.host === 'www.abcam.com') ? '471c9cd0-248c-41d7-a173-fb32d90b8729' : '4724ee22-6eaf-4880-b130-f8996e9e5d79';
-  let FRESHCHAT_HOST = (window.location.host === 'www.abcam.com') ? 'https://abcam.freshchat.com/' : 'https://abcam-sandbox.freshchat.com';
-  let FRESHCHAT_UUID = (window.location.host === 'www.abcam.com') ? '72b36250-f7cd-4488-857c-3bf672e0c6e9' : 'b800ad67-208f-4baf-ba9f-5ba75d7a6532';
+  const FRESHCHAT_TOKEN = (window.location.host === 'www.abcam.com') ? '471c9cd0-248c-41d7-a173-fb32d90b8729' : '471c9cd0-248c-41d7-a173-fb32d90b8729';
+  const FRESHCHAT_HOST = (window.location.host === 'www.abcam.com') ? 'https://abcam.freshchat.com' : 'https://abcam.freshchat.com';
+  const FRESHCHAT_UUID = (window.location.host === 'www.abcam.com') ? '72b36250-f7cd-4488-857c-3bf672e0c6e9' : '72b36250-f7cd-4488-857c-3bf672e0c6e9';
   let FRESHCHAT_NEW_SCRIPT_URL = '//eu.fw-cdn.com/12712287/710605.js';
   const freshchat_scriptTag = document.createElement('script');
   freshchat_scriptTag.type = 'text/javascript';
-  freshchat_scriptTag.src = 'https://abcam.freshchat.com/js/widget.js?t=' + Date.now();
+  freshchat_scriptTag.src = `${FRESHCHAT_HOST}/js/widget.js?t=` + Date.now();
   document.head.appendChild(freshchat_scriptTag);
-  window.fcSettings = { token:'471c9cd0-248c-41d7-a173-fb32d90b8729', host:'https://abcam.freshchat.com', uuid:'72b36250-f7cd-4488-857c-3bf672e0c6e9' };
+  window.fcSettings = { token:FRESHCHAT_TOKEN, host:FRESHCHAT_HOST, uuid:FRESHCHAT_UUID };
 }
 // freshchat -end
 
