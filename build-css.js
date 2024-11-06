94efecb49fd5f331735ccf611c1b00cf2b9f30b1
shivangi1422 <shivangi.singh@gspann.com>
2024-11-06 17:04:45 +0530

    Webinar CSS chnages

--- build-css.js
+++ build-css.js
@@ -279,8 +279,8 @@ const fileMappings = [
     wrapper: 'dynamic-cards-wrapper',
   },
   {
-    content: './eds/templates/webinars/webinar.js',
-    output: './eds/templates/webinars/webinar.css',
+    content: './eds/templates/webinar/webinar.js',
+    output: './eds/templates/webinar/webinar.css',
   },
   {
     content: './eds/blocks/speakers-info/speakers-info.js',
