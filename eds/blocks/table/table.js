da56d747b6f6568b9724dc28f6ca97c80cc7a18e
Amlan Sengupta <amlans3@gmail.com>
2024-11-08 12:47:40 +0530

    table element with search functionality

--- eds/blocks/table/table.js
+++ eds/blocks/table/table.js
@@ -1,34 +1,25 @@
 import { applyClasses, moveInstrumentation } from '../../scripts/scripts.js';
 import {
   table, tbody, td, th, thead, tr, input, div, form, label, button,
+  span,
 } from '../../scripts/dom-builder.js';
+import { decorateIcons } from '../../scripts/aem.js';
 
-// const tableEl = table({ class: 'table-auto' });
-// let instance;
-
-function handleFilter(event) {
+function handleFilter(event, tableEl) {
   event.preventDefault();
   const data = new FormData(event.target);
-  // console.log(event, data.get('modification'));
   let value = data.get('modification');
   value = value.trim();
-  const bodyEl = event.target.parentElement.querySelector('table').querySelector('tbody');
+  const bodyEl = tableEl.querySelector('tbody');
   if (value !== '') {
     const filter = value.toLowerCase();
     [...bodyEl.children].forEach((row) => {
-      [...row.children].forEach((cell) => {
-        if (cell.innerHTML.toLowerCase().includes(filter)) {
-          applyClasses(row, 'match');
-        }
-      });
-      if (!row.classList.contains('match')) {
-        row.style.display = 'none';
-      }
+      if (!row.textContent.toLowerCase().includes(filter)) row.classList.add('hidden');
+      else row.classList.remove('hidden');
     });
   } else {
     [...bodyEl.children].forEach((row) => {
-      row.classList.remove('match');
-      row.style.display = '';
+      row.classList.remove('hidden');
     });
   }
 }
@@ -38,17 +29,22 @@ function handleFilter(event) {
  * @param {Element} block
  */
 export default async function decorate(block) {
-  const filterEl = input({
-    id: 'search-filter',
-    type: 'text',
-    name: 'modification',
-    placeholder: 'Search here...',
-    class: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5',
-  });
+  const tableEl = table({ class: 'table-auto' });
+  const filterEl = div(
+    { class: 'relative' },
+    input({
+      id: 'search-filter',
+      type: 'search',
+      name: 'modification',
+      value: '',
+      placeholder: 'Search here...',
+      class: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5',
+    }),
+  );
   const formEl = form(
     {
       class: 'flex items-center gap-x-3 mb-2',
-      onsubmit: handleFilter,
+      onsubmit: (event) => handleFilter(event, tableEl),
     },
     div(
       { class: 'space-y-1' },
@@ -57,11 +53,9 @@ export default async function decorate(block) {
     ),
     button({ type: 'submit', class: 'inline-flex items-center py-2 px-5 ms-2 mt-auto text-sm font-medium text-white tracking-wide bg-teal-700 rounded-full border border-teal-700 hover:bg-teal-800' }, 'Search'),
   );
-  const tableEl = table({ class: 'table-auto' });
   const tblHead = thead();
   const tblBody = tbody();
   const header = !block.classList.contains('no-header');
-  // const searchFlag = block.classList.contains('search-filter');
   block.parentElement.classList.add(...'relative overflow-x-auto'.split(' '));
 
   [...block.children].forEach((row, i) => {
@@ -83,10 +77,10 @@ export default async function decorate(block) {
     else tblBody.append(tblRow);
   });
   tableEl.append(tblHead, tblBody);
-  // instance = tableEl;
   if (true) {
     block.replaceChildren(formEl, tableEl);
   } else {
     block.replaceChildren(tableEl);
   }
+  decorateIcons(block, 20, 20);
 }
