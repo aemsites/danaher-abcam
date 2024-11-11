f2656da6115c264b068a55c1fb666f2b0d76e490
Pardeep Gera <pardeep.gera@gspann.com>
2024-11-11 18:14:36 +0530

    table changes

--- eds/blocks/table/table.js
+++ eds/blocks/table/table.js
@@ -1,56 +1,58 @@
-import { applyClasses, moveInstrumentation } from '../../scripts/scripts.js';
+import { applyClasses, debounce, moveInstrumentation } from '../../scripts/scripts.js';
 import {
   table, tbody, td, th, thead, tr, input, div, form, label, button,
+  span,
 } from '../../scripts/dom-builder.js';
 import { decorateIcons } from '../../scripts/aem.js';
 
-function handleFilter(event, tableEl) {
+function handleSearch(event, tableEl) {
   event.preventDefault();
   const data = new FormData(event.target);
   let value = data.get('modification');
   value = value.trim();
   const bodyEl = tableEl.querySelector('tbody');
-  if (value !== '') {
-    const filter = value.toLowerCase();
-    [...bodyEl.children].forEach((row) => {
-      if (!row.textContent.toLowerCase().includes(filter)) row.classList.add('hidden');
-      else row.classList.remove('hidden');
-    });
-  } else {
-    [...bodyEl.children].forEach((row) => {
-      row.classList.remove('hidden');
-    });
-  }
+  const filter = value.toLowerCase();
+  [...bodyEl.children].forEach((row) => {
+    if (!row.textContent.toLowerCase().includes(filter) && value !== '') row.classList.add('hidden');
+    else row.classList.remove('hidden');
+  });
 }
+
+const handleClear = debounce((event) => event.target.parentElement.nextElementSibling.click(), 200);
 
 /**
  *
  * @param {Element} block
  */
 export default async function decorate(block) {
-  const tableEl = table({ class: 'table-auto' });
-  const filterEl = div(
-    { class: 'relative' },
-    input({
-      id: 'search-filter',
-      type: 'search',
-      name: 'modification',
-      value: '',
-      placeholder: 'Search here...',
-      class: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5',
-    }),
-  );
+  const tableEl = table({ class: 'table-auto w-full' });
+  const filterEl = input({
+    id: 'search-filter',
+    type: 'search',
+    name: 'modification',
+    value: '',
+    onkeyup: handleClear,
+    onsearch: handleClear,
+    class: 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5',
+  });
   const formEl = form(
     {
       class: 'flex items-center gap-x-3 mb-2',
-      onsubmit: (event) => handleFilter(event, tableEl),
+      onsubmit: (event) => handleSearch(event, tableEl),
     },
     div(
       { class: 'space-y-1' },
       label({ for: 'modification', class: 'text-sm font-semibold leading-5 text-slate-400' }, 'Enter modification'),
       filterEl,
     ),
-    button({ type: 'submit', class: 'inline-flex items-center py-2 px-5 ms-2 mt-auto text-sm font-medium text-white tracking-wide bg-teal-700 rounded-full border border-teal-700 hover:bg-teal-800' }, 'Search'),
+    button(
+      {
+        type: 'submit',
+        class: 'inline-flex items-center gap-x-2 py-2 px-4 ms-2 mt-auto text-sm font-medium text-white tracking-wide bg-teal-700 rounded-full border border-teal-700 hover:bg-teal-800',
+      },
+      span({ class: 'icon icon-search invert' }),
+      'Search',
+    ),
   );
   const tblHead = thead();
   const tblBody = tbody();
@@ -82,5 +84,6 @@ export default async function decorate(block) {
   } else {
     block.replaceChildren(tableEl);
   }
+  block.classList.add('w-full');
   decorateIcons(block, 20, 20);
 }
