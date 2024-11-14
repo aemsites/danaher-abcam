cc75715889568818619d99cdb2d9e9fa567a04b4
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2024-11-14 14:05:30 +0530

    fixed eslint issue in related-stories.js file

--- eds/blocks/related-stories/related-stories.js
+++ eds/blocks/related-stories/related-stories.js
@@ -1,10 +1,7 @@
 import ffetch from '../../scripts/ffetch.js';
-import { imageHelper, getContentType } from '../../scripts/scripts.js';
-import {
-  ul, li, p, a, div, h3, span,
-} from '../../scripts/dom-builder.js';
+import { ul } from '../../scripts/dom-builder.js';
 import { getMetadata } from '../../scripts/aem.js';
-import createArticleCard from './../dynamic-cards/articleCard.js';
+import createArticleCard from '../dynamic-cards/articleCard.js';
 
 export default async function decorate(block) {
   const pagetags = getMetadata('pagetags').split(',');
@@ -29,7 +26,7 @@ export default async function decorate(block) {
       .filter((item) => item.tags.includes(storyType) && item.tags.includes(contentType))
       .all();
   } else {
-    //articles = await ffetch(`/en-us/${templateName}/query-index.json`)
+    // articles = await ffetch(`/en-us/${templateName}/query-index.json`)
     articles = await ffetch('/en-us/stories/query-index.json')
       .filter((item) => {
         const url = new URL(getMetadata('og:url'));
@@ -46,7 +43,7 @@ export default async function decorate(block) {
           'container grid max-w-7xl w-full mx-auto gap-6 grid-cols-1 sm:grid-cols-1 sm:px-0 lg:grid-cols-3 lg:px-6 xl:px-0  justify-items-center mt-3 mb-3',
   });
   articles?.forEach((article, index) => {
-    cardList.appendChild(createArticleCard(article, index === 0,'story'));
+    cardList.appendChild(createArticleCard(article, index === 0, 'story'));
   });
   block.append(cardList);
 }
