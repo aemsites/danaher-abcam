f7b45436d41f399e48c8cee02be05306a1f40331
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2024-11-18 11:32:14 +0530

    updated changes header notification banner to added in section to test it functionality

--- eds/blocks/header-notification-banner/header-notification-banner.js
+++ eds/blocks/header-notification-banner/header-notification-banner.js
@@ -1,3 +1,5 @@
 export default function decorate(block) {
-    block.classList.add(...'bg-black text-white p-8'.split(' '));
+  // eslint-disable-next-line no-console
+  console.log(block);
+  block.classList.add(...'bg-black text-white p-8'.split(' '));
 }
