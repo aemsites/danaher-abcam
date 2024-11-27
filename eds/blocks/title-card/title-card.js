a39371515132babf4aa676a339717ad99e142c96
Naveen Ramgiri <naveen.ramgiri@gspann.com>
2024-11-27 11:10:52 +0530

    addressed the review comments

--- eds/blocks/title-card/title-card.js
+++ eds/blocks/title-card/title-card.js
@@ -9,14 +9,13 @@ export default function decorate(block) {
   block.querySelector('p').classList.add(...'text-lg tracking-[0.3px] leading-7'.split(' '));
   if (block && block.classList.contains('title-image')) {
     block.querySelector('h1').parentElement.parentElement.classList.add(...'2xl:h-[400px] w-full lg:w-1/2 lg:py-6 lg:pr-6'.split(' '));
-    block.classList.add(...'text-center-align image-full-width font-sans text-base flex flex-col gap-y-6 px-6 lg:pr-0 justify-center lg:flex-row 2xl:container 2xl:max-w-7xl mx-auto items-center'.split(' '));
+    applyClasses(block, 'text-center-align image-full-width font-sans text-base flex flex-col gap-y-6 px-6 lg:pr-0 justify-center lg:flex-row 2xl:container 2xl:max-w-7xl mx-auto items-center');
     const image = block.querySelector('picture');
     image.parentElement.parentElement.classList.add(...'w-full lg:w-1/2 my-auto'.split(' '));
     const titleImage = block.querySelector('img');
-    titleImage.classList.add(...'w-full h-48 md:h-[400px] object-cover relative 2xl:w-1/2 2xl:absolute 2xl:right-0 2xl:top-36'.split(' '));
+    applyClasses(titleImage, 'w-full h-48 md:h-[400px] object-cover relative 2xl:w-1/2 2xl:absolute 2xl:right-0 2xl:top-36');
     if (template === 'webinar') {
       const subTitleEl = block.querySelector('h4');
-      console.log(subTitleEl);
       if (subTitleEl) {
         applyClasses(subTitleEl, 'text-xs text-[#378189] bg-[#EDF6F7] font-semibold rounded capitalize py-1 px-2');
         const divEl = div({ class: 'flex md:inline-flex text-align-center items-center justify-between w-full' });
@@ -37,11 +36,11 @@ export default function decorate(block) {
       }
       const buttonEl = block.querySelector('.button-container');
       if (buttonEl) {
-        buttonEl.classList.add(...'mt-8 md:mt-12'.split(' '));
+        applyClasses(buttonEl,'mt-8 md:mt-12');
         block.querySelector('h1').parentElement.parentElement.append(buttonEl);
       }
     }
   } else {
-    block.classList.add(...'mx-auto max-w-7xl px-4 pb-4 font-sans text-base md:flex flex-col justify-center'.split(' '));
+    applyClasses(block, 'mx-auto max-w-7xl px-4 pb-4 font-sans text-base md:flex flex-col justify-center');
   }
 }
