181cb40a5a0613845d1104686c6d7a5ccc076e5d
wz914876 <sharanagouda_patil@pall.com>
2024-11-29 18:24:23 +0530

    added null checks

--- eds/blocks/speakers-info/speakers-info.js
+++ eds/blocks/speakers-info/speakers-info.js
@@ -7,23 +7,31 @@ import {
 import { applyClasses } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const pictureEl = div();
+  let pictureEl = '';
+  let titleEl = '';
+  let subTitleEl = '';
+  let descriptionEl = '';
   if (block?.querySelector('picture')) {
-    pictureEl.append(block?.querySelector('picture'));
+    pictureEl = block?.querySelector('picture');
     applyClasses(pictureEl, 'shrink-0');
     const img = pictureEl?.querySelector('img');
     createOptimizedPicture(img?.src, 'img-alt', false, [{ width: '750' }]);
     applyClasses(img, 'w-16 h-16 rounded-full');
+  }  
+  if (block?.querySelector('h5')) {
+    titleEl = block?.querySelector('h5');
+    applyClasses(titleEl, 'text-normal font-semibold text-[#071112] m-0');
+  }  
+  if(block?.querySelector('h6')){
+    subTitleEl = block?.querySelector('h6');
+    applyClasses(subTitleEl, 'text-sm font-normal leading-6 text-[#65797C] w-full m-0');
+  }  
+  if(block?.querySelector('p')){
+    descriptionEl = block?.querySelector('p');
+    applyClasses(descriptionEl, 'text-lg font-normal text-[#071112] leading-7 tracking-normal');
   }
-  const titleEl = block?.querySelector('h5');
-  applyClasses(titleEl, 'text-normal font-semibold text-[#071112] m-0');
-  const subTitleEl = block?.querySelector('h6');
-  applyClasses(subTitleEl, 'text-sm font-normal leading-6 text-[#65797C] w-full m-0');
-  const descriptionEl = block?.querySelector('p');
-  applyClasses(descriptionEl, 'text-lg font-normal text-[#071112] leading-7 tracking-normal');
-
-  const parentEl = block?.parentElement?.parentElement;
   let socialLinksEl;
+  const parentEl = block?.parentElement?.parentElement;  
   if (parentEl?.lastElementChild !== null) {
     const socialLinks = parentEl?.lastElementChild?.querySelectorAll('p > a');
     if (socialLinks) {
