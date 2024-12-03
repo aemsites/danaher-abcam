cb6b48f0f199b220e1c0e22b5d555fbb65e9f42d
wz914876 <sharanagouda_patil@pall.com>
2024-12-03 13:34:53 +0530

    updated for icon types

--- eds/blocks/downloads/downloads.js
+++ eds/blocks/downloads/downloads.js
@@ -6,24 +6,18 @@ import { applyClasses } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   console.log('Decorating downloads block : ', block);
-  // applyClasses(block, 'grid max-w-7xl w-full mx-auto gap-x-2 gap-y-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 auto-rows-max sm:px-0 mt-3 mb-3 justify-around border-b border-solid border-gray-300');
-  // const divs = document.querySelectorAll('div');
-  // divs.forEach((div) => {
-  //   if (divs.innerHTML === '' || div.textContent === '') {
-  //     div.remove();
-  //   }
-  // });
-  // [...block.children].forEach((element) => {
-  //   applyClasses(element, 'flex lg:flex-row gap-x-6 items-center');
-  //   const pEl = element.querySelector('p');
-  //   if (pEl) {
-  //     const docType = pEl.textContent.toLowerCase();
-  //     pEl.classList.add('shrink-0');
-  //     const icon = span({ class: `icon icon-${docType} size-6` });
-  //     applyClasses(pEl.parentElement, 'flex flex-row-reverse gap-x-3 mr-1');
-  //     pEl.textContent = '';
-  //     pEl.append(icon);
-  //   }
+  applyClasses(block, 'grid max-w-7xl w-full mx-auto gap-x-2 gap-y-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 auto-rows-max sm:px-0 mt-3 mb-3 justify-around border-b border-solid border-gray-300');
+  [...block.children].forEach((element) => {
+    applyClasses(element, 'flex lg:flex-row gap-x-6 items-center');    
+    if (element.querySelector('p')) {
+      const pEl = element.querySelector('p');
+      const docType = pEl.textContent.toLowerCase();
+      pEl.classList.add('shrink-0');
+      const icon = span({ class: `icon icon-${docType} size-6` });
+      applyClasses(pEl.parentElement, 'flex flex-row-reverse gap-x-3 mr-1');
+      pEl.textContent = '';
+      pEl.append(icon);
+    }
   //   const icon = span({ class: 'icon icon-arrow-down-circle' });
   //   const linkEl = element.querySelector('p > a');
   //   if (linkEl) {
@@ -41,6 +35,11 @@ export default function decorate(block) {
   //       }
   //     });
   //   }
-  // });
-  // decorateIcons(block, 32, 32);
+  });
+  block.querySelectorAll('div').forEach((divEle) => {
+    if (!divEle.textContent.trim()) {
+      divEle.remove();
+    }
+  });
+  decorateIcons(block, 32, 32);
 }
