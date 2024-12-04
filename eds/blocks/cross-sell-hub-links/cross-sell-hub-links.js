ace003ef54974ec9f339fb6a6952da74d2dd05e7
vishnu-gspann <vishnu.sanikommu@gspann.com>
2024-12-04 12:28:51 +0530

    lint issues

--- eds/blocks/cross-sell-hub-links/cross-sell-hub-links.js
+++ eds/blocks/cross-sell-hub-links/cross-sell-hub-links.js
@@ -1,14 +1,12 @@
 import {
-  div, h3, ul, li, a,
+  div, ul, li, a,
 } from '../../scripts/dom-builder.js';
 import ffetch from '../../scripts/ffetch.js';
 import { buildCollectionSchema } from '../../scripts/schema.js';
 
 export default async function decorate(block) {
-  const currentPage = window.location.pathname.split('/').pop();
-  let appGuides = await ffetch('/en-us/technical-resources/guides/query-index.json').all();
-  // .filter((item) => item.parent === currentPage)
-  // .all();
+  // const currentPage = window.location.pathname.split('/').pop();
+  let appGuides = await ffetch('/en-us/technical-resources/protocols/query-index.json').all();
 
   appGuides = appGuides.sort((item1, item2) => item1.title.localeCompare(item2.title));
   const applicationGuidesDiv = div({ class: 'pt-6 basis-1/2' }, ul());
@@ -20,22 +18,10 @@ export default async function decorate(block) {
       ),
     );
   });
-
-  // let raGuides = await ffetch('/en-us/technical-resources/guides/query-index.json')
-  //   .filter((item) => item.parent === currentPage)
-  //   .filter((item) => item.tags.includes('research-areas'))
-  //   .all();
-
-  // raGuides = raGuides.sort((item1, item2) => item1.title.localeCompare(item2.title));
-  // const raGuidesDiv = div({ class: 'pt-6 basis-1/2' }, h3('By Research area'), ul());
-  // raGuides.forEach((element) => {
-  //   raGuidesDiv.querySelector('ul')
-  // .appendChild(li({ class: 'mb-4 font-semibold text-lg text-[#378189] hover:underline' }, a({ href: element.path }, element.title.replace(/\s*\|\s*abcam$/i, ''))));
-  // });
   block.innerText = '';
   block.appendChild(div({ class: '' }, applicationGuidesDiv));
 
-  let allGuides = await ffetch('/en-us/technical-resources/guides/query-index.json')
+  let allGuides = await ffetch('/en-us/technical-resources/protocols/query-index.json')
     // .filter((item) => item.parent === currentPage)
     .all();
   allGuides = allGuides.sort((item1, item2) => item1.title.localeCompare(item2.title));
