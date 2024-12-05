7658402a1fc30fefffbe7b7818820aa61cbd5618
Amlan Sengupta <amlans3@gmail.com>
2024-12-05 23:39:41 +0530

    highlighting-text utility js, search-suggestion rendering, HTML structure implementation for facets, controller & engine implementation

--- eds/scripts/coveo/controller.js
+++ eds/scripts/coveo/controller.js
@@ -1,43 +1,40 @@
 import {
-    buildSearchBox,
-    buildResultList,
-    buildTab,
-    buildFacet,
-    buildPager,
-    buildSort,
-    SearchBox as SearchBoxType,
-  } from '@coveo/headless';
-  //import { buildSearch } from '@coveo/headless/commerce';
-  import { searchEngine, ecommerceEngine } from '../engine/engine.js';
+  buildSearchBox,
+} from 'https://static.cloud.coveo.com/headless/v3/headless.esm.js';
+import { searchEngine } from './engine.js';
 
-  const headlessSearchBox = SearchBoxType;
-  
-  export const searchBoxController = buildSearchBox(searchEngine);
-  export const searchResultListController = buildResultList(searchEngine);
-  export const selectSuggestion = buildSearchBox(headlessSearchBox.selectSuggestion('a'));
-  //export const ecommerceSearchController = buildSearch(ecommerceEngine);
-  export const pagerController = buildPager(searchEngine);
-  export const allTabController = buildTab(searchEngine, {
-    options: { expression: '', id: 'all' },
-  });
-  export const pageTabController = buildTab(searchEngine, {
-    options: { expression: '@filetype==html', id: 'html' },
-  });
-  export const videoTabController = buildTab(searchEngine, {
-    options: { expression: '@filetype==YouTubeVideo', id: 'video' },
-  });
-  // export const productsTabController = buildTab(ecommerceEngine, {
-  //   options: { expression: '', id: 'products' },
-  // });
-  export const sourceFacetController = buildFacet(searchEngine, {
-    options: { field: 'source' },
-  });
-  export const filetypeFacetController = buildFacet(searchEngine, {
-    options: { field: 'filetype' },
-  });
-  export const sortController = buildSort(searchEngine, {
-    initialState: {
-      criterion: { by: 'relevancy' },
-    },
-  });
+// const headlessSearchBox = SearchBoxType;
+
+export const searchBoxController = buildSearchBox(searchEngine, {
+  options: {
+    numberOfSuggestions: 5,
+  },
+});
+// export const searchResultListController = buildResultList(searchEngine);
+// export const selectSuggestion = buildSearchBox(headlessSearchBox.selectSuggestion('a'));
+// //export const ecommerceSearchController = buildSearch(ecommerceEngine);
+// export const pagerController = buildPager(searchEngine);
+// export const allTabController = buildTab(searchEngine, {
+//   options: { expression: '', id: 'all' },
+// });
+// export const pageTabController = buildTab(searchEngine, {
+//   options: { expression: '@filetype==html', id: 'html' },
+// });
+// export const videoTabController = buildTab(searchEngine, {
+//   options: { expression: '@filetype==YouTubeVideo', id: 'video' },
+// });
+// // export const productsTabController = buildTab(ecommerceEngine, {
+// //   options: { expression: '', id: 'products' },
+// // });
+// export const sourceFacetController = buildFacet(searchEngine, {
+//   options: { field: 'source' },
+// });
+// export const filetypeFacetController = buildFacet(searchEngine, {
+//   options: { field: 'filetype' },
+// });
+// export const sortController = buildSort(searchEngine, {
+//   initialState: {
+//     criterion: { by: 'relevancy' },
+//   },
+// });
   
--- eds/scripts/coveo/engine.js
+++ eds/scripts/coveo/engine.js
@@ -1,11 +1,11 @@
-import { buildSearchEngine, getOrganizationEndpoints } from '@coveo/headless';
-import { buildCommerceEngine } from '@coveo/headless/commerce';
+import { buildSearchEngine, getOrganizationEndpoint } from 'https://static.cloud.coveo.com/headless/v3/headless.esm.js';
+// import { buildCommerceEngine } from 'https://static.cloud.coveo.com/headless/v3/headless.esm.js/commerce';
 
 export const searchEngine = buildSearchEngine({
   configuration: {
     organizationId: 'danahernonproduction1892f3fhz',
     accessToken: 'xx26097312-c0ba-4c54-b22d-0a258570650a',
-    organizationEndpoints: getOrganizationEndpoints(
+    organizationEndpoints: getOrganizationEndpoint(
       'danahernonproduction1892f3fhz'
     ),
     search: {
@@ -15,23 +15,23 @@ export const searchEngine = buildSearchEngine({
   },
 });
 
-export const ecommerceEngine = buildCommerceEngine({
-  configuration: {
-    organizationId: 'danahernonproduction1892f3fhz',
-    accessToken: 'xx27ea823a-e994-4d71-97f6-403174ec592a',
-    analytics: {
-      trackingId: 'abcamtest',
-    },
-    organizationEndpoints: getOrganizationEndpoints(
-      'danahernonproduction1892f3fhz'
-    ),
-    context: {
-      currency: 'USD',
-      country: 'us',
-      language: 'en',
-      view: {
-        url: 'https://lifesciences.danaher.com',
-      },
-    },
-  },
-});
+// export const ecommerceEngine = buildCommerceEngine({
+//   configuration: {
+//     organizationId: 'danahernonproduction1892f3fhz',
+//     accessToken: 'xx27ea823a-e994-4d71-97f6-403174ec592a',
+//     analytics: {
+//       trackingId: 'abcamtest',
+//     },
+//     organizationEndpoints: getOrganizationEndpoints(
+//       'danahernonproduction1892f3fhz'
+//     ),
+//     context: {
+//       currency: 'USD',
+//       country: 'us',
+//       language: 'en',
+//       view: {
+//         url: 'https://lifesciences.danaher.com',
+//       },
+//     },
+//   },
+// });
