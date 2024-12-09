8098994cf98933e1f1c073528a1fce0889095b8e
Naveen Ramgiri <naveen.ramgiri@gspann.com>
2024-12-09 12:38:07 +0530

    search results block lint issue

--- eds/blocks/coveo-search-results/coveo-search-results.js
+++ eds/blocks/coveo-search-results/coveo-search-results.js
@@ -1,7 +1,5 @@
 export default function decorate(block) {
-    const para = document.createElement('p');
-    
-    para.textContent = "Hello";
-    
-    block.appendChild(para);
+  const para = document.createElement('p');
+  para.textContent = 'Hello';
+  block.appendChild(para);
 }
