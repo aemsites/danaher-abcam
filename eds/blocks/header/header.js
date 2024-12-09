7af91a0f38932ea5aad3aeab813db18926e53c83
Rajesh-GSPANN <rajesh.chithaluri@gspann.com>
2024-12-09 17:08:06 +0530

    Task 532 | Task 545 : Added else condition for the cart icon_v-2.0

--- eds/blocks/header/header.js
+++ eds/blocks/header/header.js
@@ -721,6 +721,12 @@ export default async function decorate(block) {
           console.error('There was an error making the API call:', error);
         });
     }
+    else
+    {
+      cartButton.addEventListener('click', () => {
+        window.location.href = `https://${hostName}/en-us/shopping-basket?country=${selectedCountry.toUpperCase()}`;
+      });
+    }
   }
   return block;
 }
