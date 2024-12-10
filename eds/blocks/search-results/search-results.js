5357e873af85dda0aeaf5170d271835c13f4c140
Amlan Sengupta <amlans3@gmail.com>
2024-12-10 12:00:00 +0530

    search-results.json updated

--- eds/blocks/search-results/search-results.js
+++ eds/blocks/search-results/search-results.js
@@ -1,6 +1,7 @@
 import { p } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
+  console.log('search-results');
   const para = p('Hello');
   block.appendChild(para);
 }
