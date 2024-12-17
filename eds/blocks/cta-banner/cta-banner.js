9367adcf2aeb1da41cd90cf1a5aac1aa1b3763f9
nagaraju-gspann <nagaraju.koniki@gspann.com>
2024-12-17 08:52:16 +0530

    Fixed review comments

--- eds/blocks/cta-banner/cta-banner.js
+++ eds/blocks/cta-banner/cta-banner.js
@@ -6,16 +6,16 @@ import { applyClasses } from '../../scripts/scripts.js';
 export default async function decorate(block) {
   const productFullResponse = await getProductsListResponse(1);
   const parsedJson = JSON.parse(productFullResponse || '{}');
-  block.classList.add(...'cta-container flex md:items-center flex-col md:flex-row justify-between gap-6 md:gap-8 w-full p-6 mt-10 mb-14 rounded-xl bg-[#edf6f7]'.split(' '));
+  applyClasses(block, 'cta-container flex md:items-center flex-col md:flex-row justify-between gap-6 md:gap-8 w-full p-6 mt-10 mb-14 rounded-xl bg-[#edf6f7]');
   const title = block.querySelector('h3');
   const totalCountEl = span({ class: 'font-bold text-xl text-[#378189]' }, ` ${parsedJson.totalCount ? `(${parsedJson.totalCount})` : ''}`);
   title.append(totalCountEl);
   const description = block.querySelector('p');
   const descriptionDiv = div();
   if (description && (!description.parentElement || !description.parentElement.classList.contains('button-container'))) {
-    descriptionDiv.classList.add('cta-desc', 'flex-grow', 'text-[#378189]', 'text-sm', 'break-words');
+    applyClasses(descriptionDiv, 'cta-desc flex-grow text-[#378189] text-sm break-words');
     descriptionDiv.appendChild(description);
-    description.classList.add('my-0', '!text-lg', 'font-semibold');
+    applyClasses(description, 'my-0 !text-lg font-semibold');
     title.insertAdjacentElement('afterend', descriptionDiv);
   }
   applyClasses(title, `title mt-0 ${descriptionDiv.classList.contains('cta-desc') ? 'mb-4' : 'mb-0'} !font-bold !text-xl text-[#378189] break-words`);
