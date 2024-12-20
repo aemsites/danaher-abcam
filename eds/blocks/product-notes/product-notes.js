896ee851fe40ca4d9d457addfab7d946d87e881d
wz914876 <sharanagouda_patil@pall.com>
2024-12-20 15:35:04 +0530

    css updates

--- eds/blocks/product-notes/product-notes.js
+++ eds/blocks/product-notes/product-notes.js
@@ -1,27 +1,32 @@
-import { div, hr } from '../../scripts/dom-builder.js';
+import { hr } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
   block.innerHTML = `<div class="product-notes-block">
     <div><h2>Notes</h2></div>
     <div>
-      <p>A recombinant rabbit monoclonal alternative is available to this target – ab176842</p>
-      <p>Rabbit polyclonal IgG (ab171870) is suitable for use as an isotype control with this antibody.</p>
-      <p>Abcam is leading the way to address reproducibility in scientific research with our highly validated recombinant monoclonal and recombinant multiclonal antibodies. Search & select one of Abcam's thousands of recombinant alternatives to eliminate batch-variability and unnecessary animal use.</p>
-      <p>If you do not find a host species to meet your needs, our catalogue and custom Chimeric range provides scientists the specificity of Abcam's RabMAbs in the species backbone of your choice. Remember to also review our range of edited cell lines, proteins and biochemicals relevant to your target that may help you further your research goals.</p>
-      <p>Abcam antibodies are extensively validated in a wide range of species and applications, so please check the reagent specifications meet your scientific needs before purchasing. If you have any questions or bespoke requirements, simply visit the Contact Us page to send us an inquiry or contact our Support Team ahead of purchase.</p>
+      Our RabMAb® technology is a patented hybridoma-based technology for making rabbit monoclonal antibodies. 
+      For details on our patents, please refer to <a href="/legal/rabmab-patents">RabMAb® patents</a>.
+      This product is a recombinant monoclonal antibody, which offers several advantages including:
+      <ul>
+        <li>- High batch-to-batch consistency and reproducibility</li>
+        <li>- Improved sensitivity and specificity</li>
+        <li>- Long-term security of supply</li>
+        <li>- Animal-free batch production</li>
+      </ul>
+      For more information, read more on <a href="/technical-resources/product-overview/recombinant-antibodies">recombinant antibodies</a>.
     </div>    
   </div>`;
-  const storageNotes = block.querySelector('h2');
-  const storagePEl = div();
   block.prepend(
     hr({
       class: 'h-px my-2 bg-interactive-grey-active mb-4',
     }),
   );
-  block.append(storageNotes);
-  block.querySelectorAll('p').forEach((p) => {
-    p.classList.add(...'mb-2 text-base font-normal'.split(' '));
-    storagePEl.appendChild(p);
+  const notesLinks = block.querySelectorAll('a');
+  notesLinks.forEach((link) => {
+    link.classList.add('hover:underline', 'mr-2', 'text-[#378189]');
   });
-  block.append(storagePEl);
+  const notesLists = block.querySelectorAll('ul');
+  notesLists.forEach((list) => {
+    list.classList.add('my-2', 'pl-5');
+  });
 }
