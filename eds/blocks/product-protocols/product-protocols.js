59c350b429f3585280f7ced33f6b7fd04e294524
wz914876 <sharanagouda_patil@pall.com>
2024-12-23 11:00:51 +0530

    593 [PDP] Product Protocols Block

--- eds/blocks/product-protocols/product-protocols.js
+++ eds/blocks/product-protocols/product-protocols.js
@@ -1,35 +1,29 @@
 import {
-  div,
-  a,
-  h2,
-  ul,
-  li,
+  hr,
 } from '../../scripts/dom-builder.js';
+import { applyClasses } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  block.classList.add('border-t-4');
-  const { origin } = window.location;
-  const productProtocols = div(
-    { class: 'product-protocols' },
-    h2({ class: 'mt-6 mb-4 text-2xl font-semibold tracking-tighter text-[#2A3C3C]' }, 'Product protocols'),
-    div(
-      { class: 'max-w-md font-normal' },
-      'For this product, it\'s our understanding that no specific protocols are required. You can:',
-      ul(
-        { class: 'pt-3 ml-3 list-disc list-inside text-[#071112] text-lg' },
-        li(
-          { class: 'general-protocols' },
-          'Visit the ',
-          a({ class: 'general-protocols text-[#378189] underline', href: origin.concat('/drafts/protocols') }, 'general protocols'),
-        ),
-        li(
-          { class: 'general-protocols' },
-          'Visit ',
-          a({ class: 'text-[#378189] underline', href: origin.concat('/drafts/technical-resources') }, 'troubleshooting'),
-        ),
-
-      ),
-    ),
-  );
-  block.append(productProtocols);
+  block.innerHTML = `<div class="product-protocols-block ">
+    <div><h2>Product protocols</h2></div>
+      <div>
+        <p>For this product, it's our understanding that no specific protocols are required. You can:</p>
+        <ul>
+          <li>Visit the <a href="/en-us/technical-resources/protocols">general protocols</a></li>            
+          <li>Visit <a href="/en-us/technical-resources/troubleshooting">troubleshooting</a></li>
+        </ul>
+      </div>
+    </div>`;
+  const protocolsPEl = block.querySelectorAll('p');
+  protocolsPEl.forEach((para) => {
+    applyClasses(para, 'font-normal');
+  });
+  const protocolsList = block.querySelector('ul');
+  protocolsList.querySelectorAll('li').forEach((listItem) => {
+    applyClasses(listItem, 'pt-3 ml-3 list-disc list-inside text-[#071112] text-lg');
+    listItem.querySelector('a').classList.add('text-[#378189]', 'underline');
+  });
+  block.prepend(hr({
+    class: 'h-px my-2 bg-interactive-grey-active mb-4',
+  }));
 }
