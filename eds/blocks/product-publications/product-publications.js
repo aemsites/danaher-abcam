e616bec8536cf6b18f97a10d3d6d4593db77a4e4
vishnu-gspann <vishnu.sanikommu@gspann.com>
2024-12-24 10:39:39 +0530

    Added model for testing

---
+++ eds/blocks/product-publications/product-publications.js
@@ -0,0 +1,67 @@
+import {
+  div,
+  a,
+  h2,
+  ul,
+  li,
+} from '../../scripts/dom-builder.js';
+
+export default async function decorate(block) {
+  block.innerHTML = `<div>
+                      <div class="product-publications-block ">
+                      <h3>Publications 4218</h3>
+                      <div>
+                        <div>
+                          <div>
+                            <div>Heliyon</div>
+                            <div>e23167</div>
+                          </div>
+                          <div>Inhibition of TAF1B impairs ribosome biosynthesis and suppresses cell proliferation in stomach adenocarcinoma through promoting c-MYC mRNA degradation.</div>
+                          <div></div>
+                          <div>Hang-Fei Chen,Zhang-Ping Li,Qi Wu,Chun Yu,Jing-Yi Yan,Yong-Feng Bai,Sheng-Mei Zhu,Mao-Xiang Qian,Ming Liu,Li-Feng Xu,Zheng Peng,Feng Zhang</div>
+                          <div>PubMed38169774</div>
+                        </div>
+                        <div>
+                          <div>
+                            <div>Stroke and vascular neurology</div>
+                            <div>486-502</div>
+                          </div>
+                          <div>Enhanced liver X receptor signalling reduces brain injury and promotes tissue regeneration following experimental intracerebral haemorrhage: roles of microglia/macrophages.</div>
+                          <div></div>
+                          <div>Ruiyi Zhang,Yifei Dong,Yang Liu,Dorsa Moezzi,Samira Ghorbani,Reza Mirzaei,Brian M Lozinski,Jeff F Dunn,V Wee Yong,Mengzhou Xue</div>
+                          <div>PubMed37137522</div>
+                        </div>
+                        <div>
+                          <div>
+                            <div>Stem cell research &amp; therapy</div>
+                            <div>378</div>
+                          </div>
+                          <div>Electrical stimulation affects the differentiation of transplanted regionally specific human spinal neural progenitor cells (sNPCs) after chronic spinal cord injury.</div>
+                          <div></div>
+                          <div>Nandadevi Patil,Olivia Korenfeld,Rachel N Scalf,Nicolas Lavoie,Anne Huntemer-Silveira,Guebum Han,Riley Swenson,Ann M Parr</div>
+                          <div>PubMed38124191</div>
+                        </div>
+                        <div>
+                          <div>
+                            <div>Frontiers in cellular neuroscience</div>
+                            <div>1288676</div>
+                          </div>
+                          <div>P2X7 receptor activation awakes a dormant stem cell niche in the adult spinal cord.</div>
+                          <div></div>
+                          <div>María Victoria Falco,Gabriela Fabbiani,Cecilia Maciel,Spring Valdivia,Nathalia Vitureira,Raúl E Russo</div>
+                          <div>PubMed38164435</div>
+                        </div>
+                        <div>
+                          <div>
+                            <div>Burns &amp; trauma</div>
+                            <div>tkad024</div>
+                          </div>
+                          <div>S-Nitrosylation-mediated coupling of DJ-1 with PTEN induces PI3K/AKT/mTOR pathway-dependent keloid formation.</div>
+                          <div></div>
+                          <div>Dongming Lv,Zhongye Xu,Pu Cheng,Zhicheng Hu,Yunxian Dong,Yanchao Rong,Hailin Xu,Zhiyong Wang,Xiaoling Cao,Wuguo Deng,Bing Tang</div>
+                          <div>PubMed38116467</div>
+                        </div>
+                      </div>
+                      </div>
+                    </div>`;
+}
