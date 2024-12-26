b1da29a3695f3d08b05a370aa227b00c806e707e
wz914876 <sharanagouda_patil@pall.com>
2024-12-26 14:30:22 +0530

    updated css

--- eds/blocks/product-notes/product-notes.js
+++ eds/blocks/product-notes/product-notes.js
@@ -1,26 +1,19 @@
 import { hr } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
-  block.innerHTML = `<div class="product-notes-block">
-    <div><h2>Notes</h2></div>
-    <div>
-      Our RabMAb® technology is a patented hybridoma-based technology for making rabbit monoclonal antibodies. 
-      For details on our patents, please refer to <a href="/legal/rabmab-patents">RabMAb® patents</a>.
-      This product is a recombinant monoclonal antibody, which offers several advantages including:
-      <ul>
-        <li>- High batch-to-batch consistency and reproducibility</li>
-        <li>- Improved sensitivity and specificity</li>
-        <li>- Long-term security of supply</li>
-        <li>- Animal-free batch production</li>
-      </ul>
-      For more information, read more on <a href="/technical-resources/product-overview/recombinant-antibodies">recombinant antibodies</a>.
-    </div>    
-  </div>`;
-  block.prepend(
-    hr({
-      class: 'h-px my-2 bg-interactive-grey-active mb-4',
-    }),
-  );
+  block.innerHTML = `<div class="product-notes-block notes">
+            <div>
+                <h3>Notes</h3>
+            </div>
+            <div>
+                <p>ab16667 was switched from a hybridoma to recombinant production method on 24th October 2019.</p>
+                Our RabMAb® technology is a patented hybridoma-based technology for making rabbit monoclonal antibodies. For details on our patents, please refer to <a href="/legal/rabmab-patents">RabMAb® patents</a>.
+                This product is a recombinant monoclonal antibody, which offers several advantages including:<ul><li>- High batch-to-batch consistency and reproducibility</li><li>- Improved sensitivity and specificity</li><li>- Long-term security of supply</li><li>- Animal-free batch production</li></ul>For more information, read more on <a href="/technical-resources/product-overview/recombinant-antibodies">recombinant antibodies</a>.
+                <p><strong>This product is FOR RESEARCH USE ONLY. For commercial use, please contact partnerships@abcam.com.</strong></p>
+            </div>
+        </div>`;
+  const h3El = block.querySelector('h3');
+  h3El.classList.add('mb-3');  
   const notesLinks = block.querySelectorAll('a');
   notesLinks.forEach((link) => {
     link.classList.add('hover:underline', 'mr-2', 'text-[#378189]');
@@ -29,4 +22,9 @@ export default function decorate(block) {
   notesLists.forEach((list) => {
     list.classList.add('my-2', 'pl-5');
   });
+  block.prepend(
+    hr({
+      class: 'h-px my-2 bg-interactive-grey-active mb-4',
+    }),
+  );  
 }
