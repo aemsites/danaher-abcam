af4781b769e585bf2af785b7b5a40ef369263227
Pardeep Gera <pardeep.gera@gspann.com>
2024-12-27 16:53:07 +0530

    updated timeline block

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -1,5 +1,4 @@
-import { div, button } from '../../scripts/dom-builder.js';
-import { toolTip } from '../../scripts/scripts.js';
+import { div, h5 } from '../../scripts/dom-builder.js';
 
 function getTimelineSteps() {
   const contentSections = document.querySelectorAll('[data-stepNumber]');
@@ -11,14 +10,18 @@ function getTimelineSteps() {
 }
 
 export default async function decorate(block) {
- const steps = getTimelineSteps();
- if (steps.length > 0) {
- steps.forEach((step) =>{
-  step.className = 'flex gap-x-4 mb-6';
-  const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, 1);
-  const stepDivider = div({ class: 'border border-gray-100' });
-  const stepContent = div({ class: 'flex flex-col gap-y-4 py-2' });
-
- });
-}
+  const steps = getTimelineSteps();
+  if (steps.length > 0) {
+    steps.forEach((step) => {
+      step.className = 'flex gap-x-4 mb-6';
+      const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, 1);
+      const stepDivider = div({ class: 'border border-gray-100' });
+      const stepContent = div({ class: 'flex flex-col gap-y-4 py-2' });
+      step.innerHTML = '';
+      step.prepend(stepDivider);
+      step.prepend(stepIndexElement);
+      step.append(stepContent);
+    });
+  }
+  block.innerHTML = '';
 }
