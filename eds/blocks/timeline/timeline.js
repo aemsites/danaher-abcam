b73e04db25511b065452b91bb03806ead7689746
Pardeep Gera <pardeep.gera@gspann.com>
2024-12-27 20:25:43 +0530

    updated timeline block

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -1,27 +1,30 @@
 import { div, h5 } from '../../scripts/dom-builder.js';
 
-function getTimelineSteps() {
-  const contentSections = document.querySelectorAll('[data-stepNumber]');
+export default async function decorate(block) {
+  const contentSections = document.querySelectorAll('[data-stepnumber]');
   const allSteps = new Set();
   contentSections.forEach((section) => {
-    allSteps.add(section.dataset.stepNumber);
+    allSteps.add(section.dataset.stepnumber);
   });
-  return [...allSteps];
-}
-
-export default async function decorate(block) {
-  const steps = getTimelineSteps();
-  if (steps.length > 0) {
-    steps.forEach((step) => {
-      step.className = 'flex gap-x-4 mb-6';
-      const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, 1);
+  block.innerHTML = '';
+  if (allSteps.size > 0) {
+    allSteps.forEach((step) => {
+      const stepEl = div({ class: 'flex gap-x-4 mb-6' });
+      const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, step);
       const stepDivider = div({ class: 'border border-gray-100' });
       const stepContent = div({ class: 'flex flex-col gap-y-4 py-2' });
-      step.innerHTML = '';
-      step.prepend(stepDivider);
-      step.prepend(stepIndexElement);
-      step.append(stepContent);
+      contentSections.forEach((section) => {
+        if (section.dataset.stepnumber === step) {
+          stepContent.innerHTML = section.innerHTML;
+          section.remove();
+        }
+      });
+      stepEl.innerHTML = '';
+      stepEl.prepend(stepDivider);
+      stepEl.prepend(stepIndexElement);
+      stepEl.append(stepContent);
+      block.appendChild(stepEl);
+      block.innerHTML+= '&nbsp;';
     });
   }
-  block.innerHTML = '';
 }
