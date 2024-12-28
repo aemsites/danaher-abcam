f9336f891bfb56f7453d0ec5d3f6727da85494d1
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2024-12-28 10:28:25 +0530

    updated changes for timeline-notification icon code

--- eds/blocks/timeline-notification/timeline-notification.js
+++ eds/blocks/timeline-notification/timeline-notification.js
@@ -1,13 +1,26 @@
 import { applyClasses } from '../../scripts/scripts.js';
+import { span } from '../../scripts/dom-builder.js';
+import { decorateIcons } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  const notification=block.querySelector('div:nth-of-type(1)');
+ // const icon = span({ class: 'icon icon-warning p-2.5 ' });
+  console.log(notification);
   if (block && block.classList.contains('knowledge')) {
-    applyClasses(block.querySelector('div:nth-of-type(2)'), 'py-0.5 px-1 bg-gray-300');
+    applyClasses(notification, 'flex pt-0.5 px-1 bg-gray-300');
+    const icon = span({ class: 'icon icon-knowledge p-2.5 ' });
+     notification.prepend(icon);  
   } else if (block && block.classList.contains('warning')) {
-    applyClasses(block.querySelector('div:nth-of-type(2)'), ' py-0.5 px-1 bg-orange-50');
+    applyClasses(notification, 'flex pt-0.5 px-1 bg-orange-50');
+    const icon = span({ class: 'icon icon-warning p-2.5 ' });
+    notification.prepend(icon);  
   } else if (block && block.classList.contains('help')) {
-    applyClasses(block.querySelector('div:nth-of-type(2)'), ' py-0.5 px-1 bg-green-200');
+     applyClasses(notification, 'flex pt-0.5 px-1 bg-green-200');
+     const icon = span({ class: 'icon icon-help p-2.5 ' });
+     notification.prepend(icon);  
   }
-  const description = block.querySelector('p:nth-of-type(2)');
-  if (description) description.classList.add(...'text-lg tracking-[0.3px] leading-7'.split(' '));
+ // notification.prepend(icon);  
+  decorateIcons(notification);
+  const description = block.querySelector('p:nth-of-type(1)');
+ if (description) description.classList.add(...'text-lg tracking-[0.3px] leading-7'.split(' '));
 }
