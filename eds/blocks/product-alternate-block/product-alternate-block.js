7b44eb5ad1c9c7ae1e128fb86df85585974926e2
shivangi1422 <shivangi.singh@gspann.com>
2025-01-02 20:05:35 +0530

    Changes for Alternate Block

---
+++ eds/blocks/product-alternate-block/product-alternate-block.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+  block.innerHTML = `<div>
+      <div class="product-alternate-block ">
+          <h3>Consider this alternative</h3>
+          <ul>
+             <li>
+                <a>
+                   <p></p>
+                   <p></p>
+                   <p>Anti-Histone H3 antibody [EPR16987] - Nuclear Marker and ChIP Grade</p>
+                </a>
+             </li>
+          </ul>
+      </div>
+	</div> `
+}
