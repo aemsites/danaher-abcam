4737d72d22b0d1f8b5a1e9ef0e15c1d73c18be3a
shivangi1422 <shivangi.singh@gspann.com>
2025-01-02 17:42:48 +0530

    title-card-changes

--- eds/blocks/title-card/title-card.js
+++ eds/blocks/title-card/title-card.js
@@ -8,13 +8,23 @@ export default function decorate(block) {
   const description = block.querySelector('p:nth-of-type(2)');
   if (description) description.classList.add(...'text-lg tracking-[0.3px] leading-7'.split(' '));
   if (block && block.classList.contains('title-image')) {
+    const template = getMetadata('template');
     block.parentElement.parentElement.classList.add('relative');
     block.querySelector('h1').parentElement.parentElement.classList.add(...'xl:min-h-[400px] w-full lg:w-1/2 lg:py-6 lg:pr-6'.split(' '));
     applyClasses(block, 'text-center-align image-full-width font-sans text-base flex flex-col gap-y-6 pb-4 px-6 lg:pr-0 justify-center lg:flex-row xl:container xl:max-w-7xl mx-auto items-center');
     const image = block.querySelector('picture');
-    image.parentElement.parentElement.classList.add(...'w-full lg:w-1/2 my-auto'.split(' '));
-    const titleImage = block.querySelector('img');
-    applyClasses(titleImage, 'w-full h-48 md:h-[400px] object-cover relative xl:w-1/2 xl:absolute xl:right-0 xl:top-0');
+    if (image) {
+      image.parentElement.parentElement.classList.add(...'w-full lg:w-1/2 my-auto'.split(' '));
+      const titleImage = block.querySelector('img');
+      if (template === 'cross-sell-detail') {
+        applyClasses(titleImage, 'w-full h-48 md:h-[400px] object-cover relative xl:w-1/2 xl:absolute xl:right-0 xl:top-0');
+      } else {
+        applyClasses(titleImage, 'w-full h-48 md:h-[400px] object-cover relative xl:w-1/2 xl:absolute xl:right-0 xl:top-28');
+      } 
+    } else {
+      const video = block.querySelector('a');
+      video.parentElement.parentElement.parentElement.classList.add(...'w-full lg:w-1/2 my-auto'.split(' '));
+    }
     const subTitleEl = block.querySelector('h4');
     if (subTitleEl) {
       applyClasses(subTitleEl, 'text-xs text-[#378189] bg-[#EDF6F7] font-semibold rounded py-1 px-2');
