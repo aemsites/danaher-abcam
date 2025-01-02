dd83841b7dc414a6209978265f6cce65fe183188
shivangi1422 <shivangi.singh@gspann.com>
2025-01-02 13:34:08 +0530

    changes for pathways

--- eds/templates/pathways/pathways.js
+++ eds/templates/pathways/pathways.js
@@ -1,4 +1,4 @@
-import { buildBlock } from '../../scripts/aem.js';
+import { buildBlock, getMetadata } from '../../scripts/aem.js';
 import { div, p } from '../../scripts/dom-builder.js';
 import { getCookie } from '../../scripts/scripts.js';
 
@@ -24,6 +24,8 @@ export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const allLinks = await buildRelatedLinks(main);
   const mainEl = main.querySelector('div');
+  const modifiedTime = getMetadata('modified-time');
+  const formattedDate = new Date(modifiedTime).toUTCString().slice(0, 16);
   const sectionMiddle = main.querySelector(':scope > div:nth-child(2)');
   sectionMiddle.classList.add(...'pathways-middle-container w-full pt-4'.split(' '));
   sectionMiddle.prepend(
@@ -35,6 +37,9 @@ export default async function buildAutoBlocks() {
   );
   const headerTitle = main.querySelector(':scope > div:nth-child(1)');
   headerTitle.parentNode.insertBefore(jumpToSectionDiv, headerTitle.nextSibling);
+  const defaultContainer = sectionMiddle.querySelector('div > p:first-of-type');
+  const editedTime = p({ class: 'text-[#65797C] !mb-10' }, "Last edited " + formattedDate);
+  defaultContainer.parentNode.insertBefore(editedTime, defaultContainer);
   const headerSection = div();
   const breadcrumbBlock = buildBlock('breadcrumb', { elems: [] });
   headerSection.append(breadcrumbBlock);
