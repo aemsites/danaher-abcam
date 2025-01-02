caf1198dd6a5af1e147011cc24fbd5793deb08f6
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2025-01-02 16:57:31 +0530

    added timeline-notification background colors

--- eds/blocks/timeline-notification/timeline-notification.js
+++ eds/blocks/timeline-notification/timeline-notification.js
@@ -5,15 +5,15 @@ import { decorateIcons } from '../../scripts/aem.js';
 export default function decorate(block) {
   const notification = block.querySelector('div:nth-of-type(1)');
   if (block && block.classList.contains('knowledge')) {
-    applyClasses(notification, 'flex pt-0.5 px-1 bg-gray-300');
+    applyClasses(notification, 'flex pt-0.5 px-1 bg-[#F2F2F2] rounded-lg');
     const icon = span({ class: 'icon icon-knowledge p-2.5 ' });
     notification.prepend(icon);
   } else if (block && block.classList.contains('warning')) {
-    applyClasses(notification, 'flex pt-0.5 px-1 bg-orange-50');
+    applyClasses(notification, 'flex pt-0.5 px-1 bg-orange-100 rounded-lg');
     const icon = span({ class: 'icon icon-warning p-2.5 ' });
     notification.prepend(icon);
   } else if (block && block.classList.contains('help')) {
-    applyClasses(notification, 'flex pt-0.5 px-1 bg-green-200');
+    applyClasses(notification, 'flex pt-0.5 px-1 bg-[#E8FCF4] rounded-lg');
     const icon = span({ class: 'icon icon-help p-2.5 ' });
     notification.prepend(icon);
   }
