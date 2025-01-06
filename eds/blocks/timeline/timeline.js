6f23da07f4613d652a30117f934a8ba0a641df8f
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2025-01-06 15:07:34 +0530

    updated model for timelineblock

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -2,7 +2,6 @@ import { div, h5 } from '../../scripts/dom-builder.js';
 import { applyClasses } from '../../scripts/scripts.js';
 
 export default async function decorate() {
-  console.log(document);
   const contentSections = document.querySelectorAll('[data-stepnumber]');
   contentSections.forEach((section) => {
     const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, section.dataset.stepnumber);
@@ -10,7 +9,6 @@ export default async function decorate() {
     const wrapper = document.createElement('div');
     wrapper.prepend(stepDivider);
     wrapper.prepend(stepIndexElement);
-    console.log(section.dataset.stepnumber);
     wrapper.append(...section.childNodes);
     section.append(wrapper);
     applyClasses(wrapper, 'flex gap-x-4 mb-6');
