1a6ba1fbe26936836b82e299127cdd4d10e417cf
nagaraju-gspann <nagaraju.koniki@gspann.com>
2025-01-06 16:32:20 +0530

    Fixed css issues

--- eds/blocks/accordion/accordion.js
+++ eds/blocks/accordion/accordion.js
@@ -6,11 +6,11 @@ import { applyClasses } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   [...block.children].forEach((el) => {
-    applyClasses(el, 'border-b border-b-[#D8D8D8]');
+    applyClasses(el, 'border-b border-b-[#D8D8D8] py-6 px-0');
     const contentDiv = el.querySelector('div');
     applyClasses(contentDiv, 'hidden text-base leading-[23px] tracking-[0.3px]');
     const titleDiv = div(
-      { class: 'text-[#378189] font-sans text-2xl font-semibold leading-8 tracking-[-0.5px] flex items-center cursor-pointer gap-6 justify-between py-6 px-0 m-0' },
+      { class: 'text-[#378189] font-sans text-2xl font-semibold leading-8 tracking-[-0.5px] flex items-center cursor-pointer gap-6 justify-between m-0' },
       contentDiv.querySelector('h2'),
       span({ class: 'accordion-arrow icon icon-chevron-down shrink-0 ml-auto transition' }),
     );
@@ -21,5 +21,5 @@ export default async function decorate(block) {
       else titleDiv.querySelector('.accordion-arrow').style.transform = 'rotate(180deg)';
     });
   });
-  decorateIcons(block);
+  decorateIcons(block, 20, 20);
 }
