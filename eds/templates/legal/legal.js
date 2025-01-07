7fe7465bc107589aca5ab5040769083ce9c0e8f3
Kumar Shoolam <kumar.shoolam@gspann.com>
2025-01-07 21:47:08 +0530

    Legal Page Template Creation/3.O/Lint Issues Cleared

--- eds/templates/legal/legal.js
+++ eds/templates/legal/legal.js
@@ -1,9 +1,11 @@
-import { buildBlock } from '../../scripts/aem.js';
-import { div } from '../../scripts/dom-builder.js';
+import { buildBlock, getMetadata } from '../../scripts/aem.js';
+import { div, p } from '../../scripts/dom-builder.js';
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const mainEl = main.querySelector('div');
+  const modifiedTime = getMetadata('modified-time');
+  const formattedDate = new Date(modifiedTime).toUTCString().slice(0, 16);
   const sectionMiddle = main.querySelector(':scope > div:nth-child(2)');
   sectionMiddle.classList.add(...'legal-middle-container w-full pt-4'.split(' '));
   sectionMiddle.prepend(
@@ -14,6 +16,9 @@ export default async function buildAutoBlocks() {
   );
   const headerTitle = main.querySelector(':scope > div:nth-child(1)');
   headerTitle.parentNode.insertBefore(jumpToSectionDiv, headerTitle.nextSibling);
+  const defaultContainer = sectionMiddle.querySelector('div > p:first-of-type');
+  const editedTime = p({ class: 'text-[#65797C] !mb-10' }, `Last edited ${formattedDate}`);
+  defaultContainer.parentNode.insertBefore(editedTime, defaultContainer);
   const headerSection = div();
   const breadcrumbBlock = buildBlock('breadcrumb', { elems: [] });
   headerSection.append(breadcrumbBlock);
