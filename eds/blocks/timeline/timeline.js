c9a04852f3e8fb7de36ea8bff835d131ec40c395
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2025-01-08 11:33:36 +0530

    updated timeline block for steps code

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -1,16 +1,22 @@
 import { div, h5 } from '../../scripts/dom-builder.js';
 import { applyClasses } from '../../scripts/scripts.js';
 
-export default async function decorate() {
+export default async function decorate(block) {
   const contentSections = document.querySelectorAll('[data-stepnumber]');
+  var indexNum=1;
+  const timelineStep=block.parentElement.parentElement.nextSibling.nextSibling.dataset.stepnumber; 
   contentSections.forEach((section) => {
-    const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, section.dataset.stepnumber);
+   if(timelineStep==section.dataset.stepnumber){ 
+    const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, indexNum);
     const stepDivider = div({ class: 'border border-gray-100' });
     const wrapper = document.createElement('div');
     wrapper.prepend(stepDivider);
     wrapper.prepend(stepIndexElement);
     wrapper.append(...section.childNodes);
+    console.log(section);
     section.append(wrapper);
     applyClasses(wrapper, 'flex gap-x-4 mb-6');
-  });
+    indexNum++;
+}
+});
 }
