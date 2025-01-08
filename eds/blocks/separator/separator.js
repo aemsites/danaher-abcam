3d3e86db311539aa695e93f40886450bd027c452
vijayasri palivela <vspalivela@nautilus.com>
2025-01-08 15:46:03 +0530

    added styles to the separator

--- eds/blocks/separator/separator.js
+++ eds/blocks/separator/separator.js
@@ -1,5 +1,8 @@
+import {
+  hr
+} from '../../scripts/dom-builder.js';
+
 export default function decorate(block) {
-  block.innerText = '';
-  const separator = document.createElement('hr');
+  const separator = hr({class:'mt-8 border-t border-[#07111233]'})
   block.appendChild(separator);
 }
