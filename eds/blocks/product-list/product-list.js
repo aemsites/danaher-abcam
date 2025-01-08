eadb439f7a7aae6785108cf82aaea71e60681685
Amlan Sengupta <amlans3@gmail.com>
2025-01-09 02:21:15 +0530

    taxonomy data integrated via COVEO

--- eds/blocks/product-list/product-list.js
+++ eds/blocks/product-list/product-list.js
@@ -1,12 +1,52 @@
-import { getProductsListResponse } from '../../scripts/search.js';
 import {
   a, button, div, h2, h3, p, span,
 } from '../../scripts/dom-builder.js';
 import { decorateIcons } from '../../scripts/aem.js';
+import { paginateIndexes } from '../../scripts/scripts.js';
 
-// Constants
-const pageSize = 20; // Number of records per page
-let currentPage = 1; // Current page number
+const body = {
+  trackingId: 'abcam_us',
+  clientId: '88d1fc8e-2f80-477f-b9fa-bd2e75b37c42',
+  context: {
+    user: { userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:133.0) Gecko/20100101 Firefox/133.0' },
+    view: { url: 'https://www.abcam.com' },
+    capture: true,
+    cart: [],
+    source: ['@coveo/headless@3.12.0'],
+  },
+  language: 'en',
+  country: 'us',
+  currency: 'USD',
+  page: 0,
+  facets: [],
+  sort: { sortCriteria: 'relevance' },
+};
+
+async function fetchProductList({
+  url = 'https://danahernonproduction1892f3fhz.org.coveo.com/rest/organizations/danahernonproduction1892f3fhz/commerce/v2/listing',
+  method = 'POST',
+  payload = {},
+  page = 0,
+}) {
+  try {
+    payload.page = page;
+    const request = await fetch(url, {
+      method,
+      body: JSON.stringify(payload),
+      headers: {
+        Authorization: 'Bearer xx5856f60b-8cb0-474f-9536-b8aa88df3f00',
+        'content-type': 'application/json',
+      },
+    });
+    const response = await request.json();
+
+    return response.products.length > 0 ? response : [];
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return error;
+  }
+}
 
 // Function to create product tags
 function addProductTags(productTagsArray) {
@@ -21,11 +61,9 @@ function addProductTags(productTagsArray) {
   return productTagsDiv;
 }
 
-// Function to render records on the page
-function renderRecords(records, block) {
-  block.replaceChildren(); // Clear previous content
+function renderNewRecords(records) {
   const parentContainer = div(
-    { class: 'products-parent-container flex flex-col w-full px-6' },
+    { class: 'products-parent-container flex flex-col w-full' },
     h2({ class: 'mb-6 font-bold text-2xl' }, 'Products'),
     h3({ class: 'mb-3 font-semibold text-xl' }, 'Product'),
   );
@@ -34,121 +72,68 @@ function renderRecords(records, block) {
     records.forEach((product) => {
       const productContainer = div(
         { class: 'product-container pb-4 mb-4 border-b border-gray flex flex-col' },
-        p({ class: 'font-bold text-gray-400 text-body-small' }, product.raw?.productslug.split('-').slice(-1)),
+        p({ class: 'font-bold text-gray-400 text-body-small' }, product?.clickUri.split('-').slice(-1)),
         p(
           { class: 'font-bold' },
-          a({ class: 'product-link', href: `${window.location.origin}/en-us/products/detail/${product.raw.productslug}` }, product.raw?.name),
+          a({ class: 'product-link', href: product.clickUri }, product.ec_name),
         ),
       );
       if (product.raw?.producttags) {
-        if (addProductTags !== null) {
-          productContainer.append(addProductTags(product.raw.producttags));
-        }
+        productContainer.append(addProductTags(product.ec_category));
       }
       parentContainer.append(productContainer);
     });
-    block.append(parentContainer);
   }
+  return parentContainer;
 }
-// Function to load a specific page of records
-async function loadPage(page, block) {
-  block.classList.add('px-6');
-  currentPage = page;
-  const result = (page - 1) * pageSize;
-  const productFullResponse = await getProductsListResponse(result);
-  const parsedJson = JSON.parse(productFullResponse);
-  const productsListResponse = parsedJson.results;
 
-  renderRecords(productsListResponse, block);
-  const totalRecords = parsedJson.totalCount;
-  const totalPages = Math.ceil(totalRecords / pageSize);
-
-  const paginationContainer = div({ class: 'pagination-controls flex flex-wrap justify-center items-center gap-2 mt-8' });
-  paginationContainer.replaceChildren();
-  block.append(paginationContainer); // Clear previous pagination
-
-  const prevButton = button({ class: 'previous-button cursor-not-allowed hover:bg-gray-400/30 p-2 rounded-full rotate-90', title: 'Next' }, span({ class: 'icon icon-chevron-down' }));
-  paginationContainer.appendChild(prevButton);
-
-  // Always show the first page
-  const firstPageButton = button({ class: 'first-page hover:underline hover:bg-gray-400/30 px-4 py-2 rounded-full cursor-pointer bg-gray-400/30' }, 1);
-  firstPageButton.onclick = () => loadPage(1, block);
-  paginationContainer.appendChild(firstPageButton);
-
-  // Create the Previous button
-  if (page > 1) {
-    paginationContainer.querySelector('.previous-button').classList.remove('cursor-not-allowed');
-    paginationContainer.querySelector('.first-page').classList.remove('bg-gray-400/30');
-    prevButton.onclick = () => loadPage(page - 1, block);
-  }
-
-  // Show dots after the first number if there are more pages
-  if (page > 7) {
-    const ellipsisButton = span({ class: 'ellipsis-button' }, '. . .');
-    paginationContainer.appendChild(ellipsisButton);
-  }
-
-  // Calculate the range of page numbers to show
-  const maxVisiblePages = 13;
-  const middlePage = Math.floor(maxVisiblePages / 2);
-  let startPage; let
-    endPage;
-
-  if (totalPages <= maxVisiblePages) {
-    // If total pages is less than or equal to maxVisiblePages, show all pages
-    startPage = 2;
-    endPage = totalPages - 1;
-  } else if (page <= middlePage) {
-    // If current page is in the first half
-    startPage = 2;
-    endPage = startPage + maxVisiblePages - 2;
-  } else if (page + middlePage >= totalPages) {
-    // If current page is in the last half
-    endPage = totalPages;
-    startPage = endPage - maxVisiblePages + 2;
-  } else {
-    // If current page is in the middle
-    startPage = page - middlePage + 1;
-    endPage = page + middlePage - 1;
-  }
-
-  // Show page numbers in the calculated range
-  for (let i = startPage; i <= endPage; i += 1) {
-    if (i === page) {
-      const activePageButton = button({ class: 'hover:underline px-4 py-2 rounded-full cursor-pointer bg-gray-400/30' }, i); // Highlight the active page
-      paginationContainer.appendChild(activePageButton);
-    } else {
-      const pageButton = button({ class: 'px-4 py-2 rounded-full cursor-pointer hover:underline hover:bg-gray-400/30' }, i);
-      pageButton.textContent = i;
-      pageButton.onclick = () => loadPage(i, block);
-      paginationContainer.appendChild(pageButton);
+async function loadNewPage(block, page = 0) {
+  const response = await fetchProductList({ payload: body, page });
+  const lists = response.products;
+  const content = renderNewRecords(lists, block);
+  const totalRecords = response.pagination.totalEntries;
+  if (content.children.length > 0) {
+    block.innerHTML = '';
+    block.append(content);
+    if (response.pagination.totalPages > 1) {
+      const prevBtnEl = button({
+        class: 'previous-button hover:bg-gray-400/30 p-2 rounded-full rotate-90 disabled:opacity-25 disabled:cursor-not-allowed',
+        title: 'Previous',
+        ...(response.pagination.page === 0) ? { disabled: true } : '',
+        onclick: () => loadNewPage(block, response.pagination.page - 1),
+      }, span({ class: 'icon icon-chevron-down' }));
+      const nextBtnEl = button({
+        class: 'next-button p-2 rounded-full -rotate-90 hover:bg-gray-400/30 disabled:opacity-25 disabled:cursor-not-allowed',
+        title: 'Next',
+        ...(response.pagination.page === (response.pagination.totalPages - 1)) ? { disabled: true } : '',
+        onclick: () => loadNewPage(block, response.pagination.page + 1),
+      }, span({ class: 'icon icon-chevron-down' }));
+      const paginatedArr = paginateIndexes({
+        listLength: totalRecords,
+        currentPage: response.pagination.page,
+        perPage: response.pagination.perPage,
+      });
+      const paginationContainer = div(
+        { class: 'pagination-controls flex flex-wrap justify-center items-center gap-2 mt-8' },
+        prevBtnEl,
+        ...paginatedArr.map((num) => {
+          const selectedPageIndex = ((num - 1) === response.pagination.page);
+          if (typeof num === 'string') return button({ class: 'px-4 py-2 rounded-full cursor-text' }, '...');
+          return button({
+            class: `px-4 py-2 rounded-full cursor-pointer hover:underline ${(selectedPageIndex) ? 'bg-gray-400/30' : 'hover:bg-gray-400/30'}`,
+            title: `Page ${num}`,
+            ...selectedPageIndex ? { disabled: true } : '',
+            onclick: () => loadNewPage(block, (num - 1)),
+          }, (num));
+        }),
+        nextBtnEl,
+      );
+      decorateIcons(paginationContainer);
+      block.append(paginationContainer);
     }
   }
-
-  // Show dots before the last number if there are more pages
-  if (totalPages > 1 && totalPages - page > middlePage) {
-    const ellipsisButton = span({ class: 'ellipsis-button' }, '. . .');
-    paginationContainer.appendChild(ellipsisButton);
-  }
-
-  if (totalPages !== page && totalPages - page > middlePage) {
-    const lastPageButton = button({ class: 'hover:underline hover:bg-gray-400/30 px-4 py-2 rounded-full cursor-pointer' }, totalPages);
-    lastPageButton.onclick = () => loadPage(totalPages, block);
-    paginationContainer.appendChild(lastPageButton);
-  }
-
-  // Create the Next button
-  const nextButton = button({ class: 'p-2 rounded-full -rotate-90 hover:bg-gray-400/30', title: 'Previous' }, span({ class: 'icon icon-chevron-down' }));
-  paginationContainer.appendChild(nextButton);
-  if (currentPage < totalPages) {
-    nextButton.onclick = () => loadPage(currentPage + 1, block);
-  } else {
-    nextButton.classList.add('cursor-not-allowed');
-  }
-  decorateIcons(paginationContainer);
 }
 
-// Main function to initialize the pagination
 export default async function decorate(block) {
-  loadPage(currentPage, block);
+  loadNewPage(block);
 }
