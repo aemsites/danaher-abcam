43b21bf291f8cb3be88ff479ca4625621c0a7005
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2025-01-08 11:36:37 +0530

    fixed eslint issues

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -3,20 +3,19 @@ import { applyClasses } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   const contentSections = document.querySelectorAll('[data-stepnumber]');
-  var indexNum=1;
-  const timelineStep=block.parentElement.parentElement.nextSibling.nextSibling.dataset.stepnumber; 
+  let indexNum = 1;
+  const timelineStep = block.parentElement.parentElement.nextSibling.nextSibling.dataset.stepnumber;
   contentSections.forEach((section) => {
-   if(timelineStep==section.dataset.stepnumber){ 
-    const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, indexNum);
-    const stepDivider = div({ class: 'border border-gray-100' });
-    const wrapper = document.createElement('div');
-    wrapper.prepend(stepDivider);
-    wrapper.prepend(stepIndexElement);
-    wrapper.append(...section.childNodes);
-    console.log(section);
-    section.append(wrapper);
-    applyClasses(wrapper, 'flex gap-x-4 mb-6');
-    indexNum++;
-}
-});
+    if (timelineStep === section.dataset.stepnumber) {
+      const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, indexNum);
+      const stepDivider = div({ class: 'border border-gray-100' });
+      const wrapper = document.createElement('div');
+      wrapper.prepend(stepDivider);
+      wrapper.prepend(stepIndexElement);
+      wrapper.append(...section.childNodes);
+      section.append(wrapper);
+      applyClasses(wrapper, 'flex gap-x-4 mb-6');
+      indexNum += 1;
+    }
+  });
 }
