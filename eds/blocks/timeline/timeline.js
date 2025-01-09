666b244442f0a2e01ed8caf33c501ae85f131a2f
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2025-01-09 19:15:44 +0530

    fixed eslint issues

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -4,9 +4,9 @@ import { applyClasses } from '../../scripts/scripts.js';
 export default async function decorate(block) {
   const contentSections = document.querySelectorAll('[data-stepnumber]');
   let indexNum = 1;
-  const timelineStep = block.parentElement?.parentElement?.nextSibling?.nextSibling.dataset.stepnumber;
+  const stepNo = block.parentElement?.parentElement?.nextSibling?.nextSibling.dataset.stepnumber;
   contentSections.forEach((section) => {
-    if (timelineStep === section.dataset.stepnumber) {
+    if (stepNo === section.dataset.stepnumber) {
       const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, indexNum);
       const stepDivider = div({ class: 'border border-gray-100' });
       const wrapper = document.createElement('div');
@@ -14,10 +14,6 @@ export default async function decorate(block) {
       wrapper.prepend(stepIndexElement);
       const sectionContentEl = div({ class: 'flex flex-col space-y-4' });
       sectionContentEl.append(...section.childNodes);
-      const subPoint = sectionContentEl.querySelectorAll('ul');
-      subPoint.forEach((item) => {
-       // item.classList.add(...'text-[#D8D8D8] ml-4'.split(' '));
-      });
       wrapper.append(stepIndexElement, stepDivider, sectionContentEl);
       section.append(wrapper);
       applyClasses(wrapper, 'flex gap-x-4 mb-6');
