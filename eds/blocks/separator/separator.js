689d6ebb938392de2a54460b71d086614cca9554
vijayasri palivela <vspalivela@nautilus.com>
2025-01-09 11:57:04 +0530

    lint checks

--- eds/blocks/separator/separator.js
+++ eds/blocks/separator/separator.js
@@ -1,8 +1,8 @@
 import {
-  hr
+  hr,
 } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
-  const separator = hr({class:'mt-8 border-t border-[#07111233]'})
+  const separator = hr({ class: 'mt-8 border-t border-[#07111233]' });
   block.appendChild(separator);
 }
