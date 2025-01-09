f74a88797729d501885456637319f8b177e49e42
Venkatesham Keerthi <venkatesham.keerthi@gspann.com>
2025-01-09 19:09:44 +0530

    updated timeline block changes for protocol template

--- eds/blocks/timeline/timeline.js
+++ eds/blocks/timeline/timeline.js
@@ -4,7 +4,7 @@ import { applyClasses } from '../../scripts/scripts.js';
 export default async function decorate(block) {
   const contentSections = document.querySelectorAll('[data-stepnumber]');
   let indexNum = 1;
-  const timelineStep = block.parentElement.parentElement.nextSibling.nextSibling.dataset.stepnumber;
+  const timelineStep = block.parentElement?.parentElement?.nextSibling?.nextSibling.dataset.stepnumber;
   contentSections.forEach((section) => {
     if (timelineStep === section.dataset.stepnumber) {
       const stepIndexElement = h5({ class: 'size-10 flex items-center text-lg p-3 border-2 border-black rounded-full' }, indexNum);
