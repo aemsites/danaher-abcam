b73dfa2504d57db7df12a0addb01599fb69f9837
vijayasri palivela <vspalivela@nautilus.com>
2025-01-10 10:55:56 +0530

    code changes for styling

--- eds/blocks/img-text-field/img-text-field.js
+++ eds/blocks/img-text-field/img-text-field.js
@@ -1,3 +1,5 @@
 export default function decorate(block) {
-    console.log(block);
+    const imgText = block.querySelector('p');
+    imgText.style.setProperty('color', '#65797C');
+    imgText.classList.add('py-2', 'text-sm', 'leading-normal');
 }
